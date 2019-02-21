d4648e2964edb9fdd1fdfc5947909c4d4c2988db
Shinnosuke Sawada <6warashi9@gmail.com>
2019-02-21 09:02:45 +0900

    initial commit

---
+++ .config/fish/config.fish
@@ -0,0 +1,30 @@
+if status is-interactive; and not set -q TMUX
+    exec direnv exec / tmux attach
+end
+set fish_complete_path /usr/local/share/fish/vendor_completions.d $fish_complete_path
+
+set -x PATH $HOME/.anyenv/bin $PATH
+set -x PATH $HOME/.cargo/bin $PATH
+set -x PATH $HOME/go/bin $PATH
+set -x PATH $HOME/.ghg/bin $PATH
+set -x PATH $HOME/.local/bin $PATH
+
+set -x FZF_TMUX 1
+set -x FZF_HISTORY_SYNC 1
+set -x FZF_DEFAULT_OPTS --reverse
+
+set -x PIPENV_VENV_IN_PROJECT true
+
+set -x EDITOR nvim
+
+alias v nvim
+alias ls exa
+alias git hub
+alias g git
+alias p gopass
+alias tmux 'direnv exec / tmux'
+
+if status --is-interactive
+    source (anyenv init -|psub)
+    source (direnv hook fish|psub)
+end
