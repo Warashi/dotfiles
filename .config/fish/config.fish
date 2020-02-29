6ae7633c458cdcff2d569fbcf5c8672d749ba5d0
Shinnosuke Sawada <6warashi9@gmail.com>
2020-02-29 15:27:19 +0900

    EDITOR環境変数の引数をちゃんと処理してくれないやつがいるのでworkaroundした

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -15,7 +15,7 @@ set -x FZF_HISTORY_SYNC 1
 set -x FZF_DEFAULT_OPTIONS "--reverse --color dark --height 10"
 set -x GHQ_OPTIONS "--vcs=git"
 
-set -x EDITOR "subl -w"
+set -x EDITOR EDITOR.sh
 
 alias k kubectl
 alias v vim
