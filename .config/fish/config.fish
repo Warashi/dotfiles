6b93496fbed42470b9bbe7bbe8da8d048d7da4bf
Shinnosuke Sawada <6warashi9@gmail.com>
2020-10-18 23:28:11 +0900

    add fish configs

---
+++ .config/fish/config.fish
@@ -0,0 +1,30 @@
+set fish_complete_path /usr/local/share/fish/vendor_completions.d $fish_complete_path
+
+set -x GOPATH $HOME/go
+
+set -x PATH /usr/local/sbin $PATH
+set -x PATH /usr/local/opt/texinfo/bin $PATH
+set -x PATH $HOME/.cargo/bin $PATH
+set -x PATH $GOPATH/bin $PATH
+set -x PATH $HOME/.local/bin $PATH
+
+set -x FZF_CMD sk
+set -x FZF_HISTORY_SYNC 1
+set -x FZF_DEFAULT_OPTIONS "--reverse --color dark --height 10"
+set -x GHQ_OPTIONS "--vcs=git"
+set -x PIPENV_VENV_IN_PROJECT 1
+
+set -x EDITOR EDITOR.sh
+
+alias k kubectl
+alias kx kubectx
+alias s subl
+alias m smerge
+alias ls exa
+alias git hub
+alias g git
+alias tmux 'direnv exec / tmux'
+
+if status --is-interactive
+    eval (direnv hook fish)
+end
---
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -0,0 +1,4 @@
+function fish_user_key_bindings
+    bind \cx '__fzf_ghq'
+    bind \cr '__fzf_history'
+end
