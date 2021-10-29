cf231712884b23f24e6dd1bd45691d83b3e44e4a
Shinnosuke Sawada <6warashi9@gmail.com>
2021-10-29 09:52:21 +0900

    add emacs bindings to insert mode

--- .config/fish/functions/fish_user_key_bindings.fish
+++ .config/fish/functions/fish_user_key_bindings.fish
@@ -1,4 +1,6 @@
 function fish_user_key_bindings
     bind -M insert \cx '__fzf_ghq'
     bind -M insert \cr '__fzf_history'
+    fish_default_key_bindings -M insert
+    fish_vi_key_bindings --no-erase insert
 end
