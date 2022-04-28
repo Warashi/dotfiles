5cce503e19a4ee6b3c7ab0824c3c9652d81f647a
Shinnosuke Sawada <6warashi9@gmail.com>
2022-04-28 21:59:50 +0900

    feat: neovim-remoteで開くときはtabnewする

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -63,9 +63,9 @@ end)
 vim.o.t_8f = '\\<Esc>[38;2;%lu;%lu;%lum' -- 文字色
 vim.o.t_8b = '\\<Esc>[48;2;%lu;%lu;%lum' -- 背景色 
 vim.g.mapleader = ',' 
-vim.env.EDITOR = 'nvr'
-vim.env.TIG_EDITOR = 'nvr'
-vim.env.GIT_EDITOR = 'nvr --remote-wait-silent'
+vim.env.EDITOR = 'nvr -cc tabnew'
+vim.env.TIG_EDITOR = 'nvr -cc tabnew'
+vim.env.GIT_EDITOR = 'nvr -cc tabnew --remote-wait-silent'
 vim.cmd([[ autocmd FileType gitcommit,gitrebase,gitconfig set bufhidden=delete ]])
 vim.cmd([[ autocmd TermOpen * startinsert ]])
 vim.cmd([[ autocmd BufLeave term://* stopinsert ]])
