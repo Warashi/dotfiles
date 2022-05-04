ae94485817b5a00b6e880476b8e3f3f730c2b119
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-04 13:34:50 +0900

    feat: <leader>a でaerial bufferに移動するように

--- .config/nvim/lua/rc/lsp.lua
+++ .config/nvim/lua/rc/lsp.lua
@@ -16,7 +16,7 @@ require('aerial').setup {
   show_guides = true,
   on_attach = function(bufnr)
     -- Toggle the aerial window with <leader>a
-    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>a', '<cmd>AerialToggle!<CR>', {})
+    vim.api.nvim_buf_set_keymap(bufnr, 'n', '<leader>a', '<cmd>AerialOpen<CR>', {})
     -- Jump forwards/backwards with '{' and '}'
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '{', '<cmd>AerialPrev<CR>', {})
     vim.api.nvim_buf_set_keymap(bufnr, 'n', '}', '<cmd>AerialNext<CR>', {})
