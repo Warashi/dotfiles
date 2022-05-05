93d240343df07293cd49b1b0dad4c4fce0521901
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-05 10:20:57 +0900

    feat: CamelCaseMotion を導入

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -52,6 +52,7 @@ require('packer').startup(function(use)
   use {'folke/which-key.nvim', config = function() require('which-key').setup {} end}
   use {'stevearc/aerial.nvim', config = function() require('rc/aerial') end}
   use {'kevinhwang91/nvim-hclipboard', config = function() require('hclipboard').start() end}
+  use {'bkad/CamelCaseMotion', config = function() require('rc/camel-case-motion') end}
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
