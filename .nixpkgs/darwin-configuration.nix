9b6534bc87df47ffde2dc0b4a64c3fab2d9581f7
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-07 16:21:50 +0900

    feat: use nix-darwin

---
+++ .nixpkgs/darwin-configuration.nix
@@ -0,0 +1,63 @@
+{ config, pkgs, ... }:
+
+{
+  # import home-manager
+  imports = [ <home-manager/nix-darwin> ];
+
+  # user config
+  users.users.sawada = {
+    name = "sawada";
+    home = "/Users/sawada";
+  };
+  home-manager.useUserPackages = true;
+  home-manager.users.sawada = { pkgs, ... }: {
+    home.packages = with pkgs; [
+      exa
+      fd
+      gh
+      ghq
+      git
+      go_1_18
+      jq
+      mas
+      neovim
+      neovim-remote
+      reattach-to-user-namespace
+      ripgrep
+      skim
+      tig
+      tmux
+    ];
+
+    programs.direnv.enable = true;
+    programs.direnv.nix-direnv.enable = true;
+    programs.home-manager.enable = true;
+  };
+
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages = [];
+  environment.shells = [ pkgs.fish ];
+  environment.variables = {
+    EDITOR = "nvim";
+  };
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  nix.package = pkgs.nixUnstable;
+  nix.extraOptions = ''
+    keep-outputs = true
+    keep-derivations = true
+  '';
+  # Create /etc/bashrc that loads the nix-darwin environment.
+  # programs.zsh.enable = true;  # default shell on catalina
+  programs.fish.enable = true;
+
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}
