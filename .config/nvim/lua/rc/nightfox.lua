9450ed2181301beb6d25ab34450fd27a44c03bba
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-08 09:41:14 +0900

    feat: change nightfox config

--- .config/nvim/lua/rc/nightfox.lua
+++ .config/nvim/lua/rc/nightfox.lua
@@ -1,11 +1,14 @@
-require('nightfox').init {
-  transparent = true
+require('nightfox').setup {
+  options = {
+    transparent = false,
+    terminal_colors = true,
+    dim_inactive = true,
+  },
 }
 vim.cmd([[ colorscheme nordfox ]])
 
-
 require('lualine').setup {
   options = {
-    theme = 'nordfox'
-  }
+    theme = 'nordfox',
+  },
 }
