e32102e7ef8145cdaad8a8c1eccac7f75ea2c011
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-10 22:31:04 +0000

    feat: refactor

--- .config/nvim/lua/rc/lsp.lua
+++ .config/nvim/lua/rc/lsp.lua
@@ -1,6 +1,15 @@
 ---- nvim-lsp-installer setup ----
 require('nvim-lsp-installer').setup {}
+
+---- null-ls setup ----
 require('null-ls').setup { sources = { require('null-ls').builtins.completion.spell } }
+
+-- Setup Go organize imports
+require('rc/goimports')
+
+-- Setup nvim-cmp.
+require('rc/cmp')
+
 ---- lsp-settings ----
 -- Mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
@@ -51,62 +60,3 @@ for _, lsp in pairs(servers) do
   }
 end
 
--- Setup Go organize imports
-require('rc/goimports')
-
--- Setup nvim-cmp.
-local cmp = require('cmp')
-
-cmp.setup({
-  snippet = {
-    -- REQUIRED - you must specify a snippet engine
-    expand = function(args)
-      vim.fn['vsnip#anonymous'](args.body)
-    end,
-  },
-  window = {
-    completion = cmp.config.window.bordered(),
-    documentation = cmp.config.window.bordered(),
-  },
-  mapping = cmp.mapping.preset.insert({
-    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete(),
-    ['<C-e>'] = cmp.mapping.abort(),
-    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-  }),
-  sources = cmp.config.sources({
-    { name = 'nvim_lsp' },
-    { name = 'emoji' },
-    { name = 'vsnip' },
-  }, {
-    { name = 'buffer' },
-  })
-})
-
--- Set configuration for specific filetype.
-cmp.setup.filetype('gitcommit', {
-  sources = cmp.config.sources({
-    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-  }, {
-    { name = 'buffer' },
-  })
-})
-
--- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline('/', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = {
-    { name = 'buffer' }
-  }
-})
-
--- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
-cmp.setup.cmdline(':', {
-  mapping = cmp.mapping.preset.cmdline(),
-  sources = cmp.config.sources({
-    { name = 'path' }
-  }, {
-    { name = 'cmdline' }
-  })
-})
