74fe933c29f6831ac918f4469ff9957ee97e6c95
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-12 22:50:35 +0000

    style: foramt lua

--- .config/nvim/lua/rc/goimports.lua
+++ .config/nvim/lua/rc/goimports.lua
@@ -1,6 +1,6 @@
 function organizeImports(wait_ms)
   local params = vim.lsp.util.make_range_params()
-  params.context = {only = {"source.organizeImports"}}
+  params.context = { only = { "source.organizeImports" } }
   local result = vim.lsp.buf_request_sync(0, "textDocument/codeAction", params, wait_ms)
   for _, res in pairs(result or {}) do
     for _, r in pairs(res.result or {}) do
