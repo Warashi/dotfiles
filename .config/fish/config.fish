d418296847449995ff3112bd3de3283fc756fc98
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-12 11:00:29 +0000

    style: indent fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,17 +1,17 @@
 set -x SHELL (which fish)
 if ! set -q HOMEBREW_SETUP_DONE
-  # homebrew
-  ## Intel
-  test -x /usr/local/bin/brew && /usr/local/bin/brew shellenv | source
-  ## M1
-  test -x /opt/homebrew/bin/brew && /opt/homebrew/bin/brew shellenv | source
-  
-  set -x HOMEBREW_SETUP_DONE 1
+    # homebrew
+    ## Intel
+    test -x /usr/local/bin/brew && /usr/local/bin/brew shellenv | source
+    ## M1
+    test -x /opt/homebrew/bin/brew && /opt/homebrew/bin/brew shellenv | source
+
+    set -x HOMEBREW_SETUP_DONE 1
 end
 
-if status --is-interactive 
-  ! set -q TMUX ;and type -q tmux ;and exec direnv exec / tmux -CC new-session -t 0
-  ! set -q REATTACHED ;and type -q reattach-to-user-namespace; and exec env REATTACHED=1 reattach-to-user-namespace -l $SHELL
+if status --is-interactive
+    ! set -q TMUX; and type -q tmux; and exec direnv exec / tmux -CC new-session -t 0
+    ! set -q REATTACHED; and type -q reattach-to-user-namespace; and exec env REATTACHED=1 reattach-to-user-namespace -l $SHELL
 end
 
 set fish_complete_path /usr/local/share/fish/vendor_completions.d $fish_complete_path
@@ -19,7 +19,7 @@ set fish_complete_path /usr/local/share/fish/vendor_completions.d $fish_complete
 fish_add_path -g -m /usr/local/sbin
 fish_add_path -g -m /usr/local/opt/texinfo/bin
 fish_add_path -g -m $HOME/.cargo/bin
-type -q go && fish_add_path -g -m (go env GOPATH)/bin 
+type -q go && fish_add_path -g -m (go env GOPATH)/bin
 fish_add_path -g -m $HOME/.local/bin
 
 set -x FZF_CMD sk
@@ -33,9 +33,9 @@ set -x ORG_JETBRAINS_PROJECTOR_SERVER_AUTO_KEYMAP false
 set -x ORG_JETBRAINS_PROJECTOR_SERVER_PIXEL_PER_UNIT 30
 
 if set -q EDITOR
-  set -x EDITOR (echo $EDITOR | string split ' ')
+    set -x EDITOR (echo $EDITOR | string split ' ')
 else
-  set -x EDITOR nvim
+    set -x EDITOR nvim
 end
 
 abbr -a -g k kubectl
@@ -49,11 +49,11 @@ alias f 'e -c ":Neotree $(pwd)"'
 alias e '$EDITOR'
 
 if status --is-login
-  type -q pyenv ;and source (pyenv init --path | psub)
+    type -q pyenv; and source (pyenv init --path | psub)
 end
 if status --is-interactive
-  type -q direnv ;and direnv hook fish | source
-  type -q pyenv ;and pyenv init - | source
-  type -q jump; ;and jump shell fish | source
-  type -q starship ;and starship init fish | source
+    type -q direnv; and direnv hook fish | source
+    type -q pyenv; and pyenv init - | source
+    type -q jump; and jump shell fish | source
+    type -q starship; and starship init fish | source
 end
