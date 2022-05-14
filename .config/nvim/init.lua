5f6267577ac9e23be94eec9979d9b0fa4de729e7
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-14 14:41:44 +0900

    style: format with lsp

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -19,5 +19,5 @@ vim.opt.shiftwidth = 2
 vim.opt.clipboard = 'unnamedplus'
 vim.opt.completeopt = 'menu,menuone,noselect'
 
-vim.keymap.set('n', '<leader><leader>', ':source $MYVIMRC<CR>', { silent=true })
-vim.keymap.set('t', '<ESC>', '<C-\\><C-n>', { silent=true })
+vim.keymap.set('n', '<leader><leader>', ':source $MYVIMRC<CR>', { silent = true })
+vim.keymap.set('t', '<ESC>', '<C-\\><C-n>', { silent = true })
