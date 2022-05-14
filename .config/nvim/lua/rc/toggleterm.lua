747d0e8294f93385663155c05fa544e26e4423de
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-14 14:07:03 +0900

    feat: add keybind to toggle tig window

--- .config/nvim/lua/rc/toggleterm.lua
+++ .config/nvim/lua/rc/toggleterm.lua
@@ -2,3 +2,12 @@ require('toggleterm').setup {
   open_mapping = [[<c-\>]],
   direction = 'float',
 }
+
+local Terminal = require('toggleterm.terminal').Terminal
+local tig      = Terminal:new({ cmd = 'tig', hidden = true })
+
+function _tig_toggle()
+  tig:toggle()
+end
+
+vim.api.nvim_set_keymap('n', '<leader>g', '<cmd>lua _tig_toggle()<CR>', { noremap = true, silent = true })
