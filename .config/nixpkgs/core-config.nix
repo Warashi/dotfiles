31d1b723864ac83c1dd691d892cd887a326ca712
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-21 06:55:03 +0900

    feat: update config

--- .config/nixpkgs/core-config.nix
+++ .config/nixpkgs/core-config.nix
@@ -1,4 +1,4 @@
-{ programs, ... }: {
+{ programs, pkgs, ... }: {
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
 
@@ -8,7 +8,6 @@
     dotDir = ".config/zsh";
     enableAutosuggestions = true;
     enableSyntaxHighlighting = true;
-    defaultKeymap = "viins";
 
     history = {
       extended = true;
@@ -16,25 +15,65 @@
     };
 
     sessionVariables = {
-      EDITOR = "nvim";
+      EDITOR = "nvr -cc ToggleTermClose --remote-wait-silent";
     };
 
     shellAliases = {
-      e = "$EDITOR";
+      e = "nvr -s -cc ToggleTermClose";
+      t = "tmux new-session -t 0";
+      g = "git";
       ls = "exa";
+      ll = "ls -l";
+      la = "ll -a";
+      f = ''e -c ":Neotree $(pwd)"'';
     };
   };
 
-  # direnv integration
+  programs.tmux = {
+    enable = true;
+    baseIndex = 1;
+    clock24 = true;
+    escapeTime = 0;
+    keyMode = "vi";
+    shortcut = "Space";
+    shell = "${pkgs.zsh}/bin/zsh";
+    terminal = "screen-256color";
+    plugins = with pkgs; [
+      tmuxPlugins.nord
+    ];
+    extraConfig = ''
+      # C-w で window 一覧を開く
+      bind C-w choose-tree -Zw
+      # C-c でwindow作成
+      bind C-c new-window
+      # C-t で現在のwindowを一番左へ移動
+      bind C-t move-window -t 0
+      # C-h, C-v で画面分割
+      bind C-h split-window -h
+      bind C-v split-window -v
+
+      # C-o, M-o で分割した画面をRotate
+      bind -r C-o rotate-window -D
+      bind -r M-o rotate-window -U
+
+      # vim っぽいキーバインドでpaneを移動
+      bind -r h select-pane -L
+      bind -r j select-pane -D
+      bind -r k select-pane -U
+      bind -r l select-pane -R
+    '';
+  };
+
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
     enableZshIntegration = true;
   };
 
-  programs.skim = {
+  programs.fzf = {
     enable = true;
     enableZshIntegration = true;
+    tmux.enableShellIntegration = true;
   };
 
   programs.starship = {
