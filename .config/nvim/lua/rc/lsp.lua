303415e0271dec95455c1caacd1510f63b9e31e1
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-21 12:01:02 +0900

    feat: use rnix language server

--- .config/nvim/lua/rc/lsp.lua
+++ .config/nvim/lua/rc/lsp.lua
@@ -17,7 +17,7 @@ end
 
 local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-local servers = { 'pyright', 'rust_analyzer', 'tsserver', 'gopls', 'sumneko_lua', 'clangd' }
+local servers = { 'pyright', 'rust_analyzer', 'tsserver', 'gopls', 'sumneko_lua', 'clangd', 'rnix' }
 for _, lsp in pairs(servers) do
   lspconfig[lsp].setup {
     on_attach = on_attach,
