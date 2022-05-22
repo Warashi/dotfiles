b673066cb2ac28db4d8ba0ca91784509be8e351d
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-22 11:23:44 +0900

    style: format with lsp

--- .config/nixpkgs/home.nix
+++ .config/nixpkgs/home.nix
@@ -3,15 +3,15 @@
 {
   imports = [ ./ubuntu.nix ./core-config.nix ];
   home.packages = with pkgs; [ docker-client unzip ]
-                    ++ import ./core-packages.nix { pkgs = pkgs; };
+    ++ import ./core-packages.nix { pkgs = pkgs; };
 
-# This value determines the Home Manager release that your
-# configuration is compatible with. This helps avoid breakage
-# when a new Home Manager release introduces backwards
-# incompatible changes.
-#
-# You can update Home Manager without changing this value. See
-# the Home Manager release notes for a list of state version
-# changes in each release.
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
   home.stateVersion = "22.05";
 }
