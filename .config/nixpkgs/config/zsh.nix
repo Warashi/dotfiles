3cd747f573db18337d7baa9f8ab77c4042c4b337
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-23 07:11:02 +0900

    fix: 使っていないので消す

--- .config/nixpkgs/config/zsh.nix
+++ .config/nixpkgs/config/zsh.nix
@@ -28,18 +28,5 @@
       [[ ! -v ZELLIJ ]] && whence zellij > /dev/null && exec direnv exec / zellij attach --create
       [[ ! -v REATTACHED ]] && whence reattach-to-user-namespace > /dev/null && exec env REATTACHED=1 reattach-to-user-namespace -l $SHELL
     '';
-
-    plugins = with pkgs; [
-      {
-        name = "zsh-fzf-ghq";
-        file = "zsh-fzf-ghq.plugin.zsh";
-        src = fetchFromGitHub {
-          owner = "subaru-shoji";
-          repo = "zsh-fzf-ghq";
-          rev = "850ad834b1b140886ded159947b4b8d5588757e6";
-          sha256 = "SsRWsA0JuRC4e9ojwt1MQEdFU1tad0Sr2K4ajHjXDTY=";
-        };
-      }
-    ];
   };
 }
