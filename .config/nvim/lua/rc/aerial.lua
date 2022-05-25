050ee2f2859679db269ac11f9579de2ee830fef0
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-26 07:34:54 +0900

    style: format with lsp

--- .config/nvim/lua/rc/aerial.lua
+++ .config/nvim/lua/rc/aerial.lua
@@ -5,12 +5,12 @@ require('aerial').setup {
   show_guides = true,
   on_attach = function(bufnr)
     -- Toggle the aerial window with <leader>a
-    vim.keymap.set('n', '<leader>a', '<cmd>AerialOpen<CR>', { buffer=true })
+    vim.keymap.set('n', '<leader>a', '<cmd>AerialOpen<CR>', { buffer = true })
     -- Jump forwards/backwards with '{' and '}'
-    vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer=true })
-    vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer=true })
+    vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = true })
+    vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = true })
     -- Jump up the tree with '[[' or ']]'
-    vim.keymap.set('n', '[[', '<cmd>AerialPrevUp<CR>', { buffer=true })
-    vim.keymap.set('n', ']]', '<cmd>AerialNextUp<CR>', { buffer=true })
+    vim.keymap.set('n', '[[', '<cmd>AerialPrevUp<CR>', { buffer = true })
+    vim.keymap.set('n', ']]', '<cmd>AerialNextUp<CR>', { buffer = true })
   end
 }
