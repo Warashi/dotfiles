c0307ff8578c37d4f01824abcba8dafbbabaccb3
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-28 16:25:11 +0900

    feat: leader + a で aerial の window が閉じるようにした

--- .config/nvim/lua/rc/aerial.lua
+++ .config/nvim/lua/rc/aerial.lua
@@ -5,7 +5,7 @@ require('aerial').setup {
   show_guides = true,
   on_attach = function(bufnr)
     -- Toggle the aerial window with <leader>a
-    vim.keymap.set('n', '<leader>a', '<cmd>AerialOpen<CR>', { buffer = true })
+    vim.keymap.set('n', '<leader>a', '<cmd>AerialToggle<CR>', { buffer = true })
     -- Jump forwards/backwards with '{' and '}'
     vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = true })
     vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = true })
