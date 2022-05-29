dc7b5af8481305c97c7eca93fb419860135bb7ea
Shinnosuke Sawada <6warashi9@gmail.com>
2022-05-29 09:34:44 +0900

    feat: use solarized theme

--- .config/nixpkgs/config/zellij.nix
+++ .config/nixpkgs/config/zellij.nix
@@ -4,19 +4,34 @@
     settings = {
       default_mode = "locked";
       copy_command = "muscat copy";
-      theme = "nord";
-      themes.nord = {
-        fg = [ 216 222 233 ]; #D8DEE9
-        bg = [ 46 52 64 ]; #2E3440
-        black = [ 59 66 82 ]; #3B4252
-        red = [ 191 97 106 ]; #BF616A
-        green = [ 163 190 140 ]; #A3BE8C
-        yellow = [ 235 203 139 ]; #EBCB8B
-        blue = [ 129 161 193 ]; #81A1C1
-        magenta = [ 180 142 173 ]; #B48EAD
-        cyan = [ 136 192 208 ]; #88C0D0
-        white = [ 229 233 240 ]; #E5E9F0
-        orange = [ 208 135 112 ]; #D08770
+      theme = "solarized";
+      themes = {
+        solarized = {
+          fg = [ 253 246 227 ];
+          bg = [ 0 43 54 ];
+          black = [ 7 54 66 ];
+          red = [ 220 50 47 ];
+          green = [ 133 153 0 ];
+          yellow = [ 181 137 0 ];
+          blue = [ 38 139 210 ];
+          magenta = [ 211 54 130 ];
+          cyan = [ 42 161 152 ];
+          white = [ 238 232 213 ];
+          orange = [ 203 75 22 ];
+        };
+        nord = {
+          fg = [ 216 222 233 ]; #D8DEE9
+          bg = [ 46 52 64 ]; #2E3440
+          black = [ 59 66 82 ]; #3B4252
+          red = [ 191 97 106 ]; #BF616A
+          green = [ 163 190 140 ]; #A3BE8C
+          yellow = [ 235 203 139 ]; #EBCB8B
+          blue = [ 129 161 193 ]; #81A1C1
+          magenta = [ 180 142 173 ]; #B48EAD
+          cyan = [ 136 192 208 ]; #88C0D0
+          white = [ 229 233 240 ]; #E5E9F0
+          orange = [ 208 135 112 ]; #D08770
+        };
       };
       ui.pane_frames.rounded_corners = true;
 
