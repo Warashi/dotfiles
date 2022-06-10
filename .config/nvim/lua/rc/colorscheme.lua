67096020257b96de8925833d1d17f2c0880aa272
Shinnosuke Sawada <6warashi9@gmail.com>
2022-06-10 11:46:00 +0900

    feat: gruvbox dark

--- .config/nvim/lua/rc/colorscheme.lua
+++ .config/nvim/lua/rc/colorscheme.lua
@@ -1,2 +1,6 @@
-vim.opt.background = 'light'
+vim.opt.background = 'dark'
+vim.g.gruvbox_contrast_dark = 'soft'
+vim.g.gruvbox_contrast_light = 'soft'
+vim.g.gruvbox_improved_strings = 1
+vim.g.gruvbox_improved_warnings = 1
 vim.cmd([[colorscheme gruvbox]])
