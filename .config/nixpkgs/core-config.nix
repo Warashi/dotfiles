b541008443e60f9b8c234d72d6384df6146c8751
Shinnosuke Sawada <6warashi9@gmail.com>
2022-06-28 16:32:06 +0900

    tmuxの設定を復活

--- .config/nixpkgs/core-config.nix
+++ .config/nixpkgs/core-config.nix
@@ -17,6 +17,48 @@
     initExtra = "exec zsh --login";
   };
 
+  programs.tmux = {
+    enable = true;
+    baseIndex = 1;
+    clock24 = true;
+    escapeTime = 0;
+    keyMode = "vi";
+    shortcut = "Space";
+    shell = "${pkgs.zsh}/bin/zsh";
+    terminal = "screen-256color";
+    plugins = with pkgs; [
+      tmuxPlugins.nord
+    ];
+    extraConfig = ''
+      # C-w で window 一覧を開く
+      bind C-w choose-tree -Zw
+
+      # C-c でwindow作成
+      bind C-c new-window
+
+      # C-t で現在のwindowを一番左へ移動
+      bind C-t move-window -t 0
+
+      # C-h, C-v で画面分割
+      bind C-h split-window -h
+      bind C-v split-window -v
+
+      # H, V で pane 再配置
+      bind H select-layout main-horizontal
+      bind V select-layout main-vertical
+
+      # C-o, M-o で分割した画面をRotate
+      bind -r C-o rotate-window -D
+      bind -r M-o rotate-window -U
+
+      # vim っぽいキーバインドでpaneを移動
+      bind -r h select-pane -L
+      bind -r j select-pane -D
+      bind -r k select-pane -U
+      bind -r l select-pane -R
+    '';
+  };
+
   programs.direnv = {
     enable = true;
     nix-direnv.enable = true;
