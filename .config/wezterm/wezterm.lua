0d567c724c8d4cbc9ccc6528dee1e37db027efab
Shinnosuke Sawada <6warashi9@gmail.com>
2022-06-29 19:13:14 +0900

    feat: change wezterm config

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,8 +2,13 @@ local wezterm = require 'wezterm';
 return {
   font = wezterm.font("UDEV Gothic NFLG"),
   font_size = 13,
+  line_height = 1.2,
   color_scheme = "nord",
-  enable_tab_bar = false,
+  enable_tab_bar = true,
   enable_scroll_bar = false,
   scrollback_lines = 0,
+  window_frame = {
+    font = wezterm.font({family="UDEV Gothic NFLG", weight="Bold"}),
+    font_size = 13,
+  },
 }
