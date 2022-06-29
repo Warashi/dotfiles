72509e4f50d6fc8d0c3adb01aa7f68014ed2ef80
Shinnosuke Sawada <6warashi9@gmail.com>
2022-06-29 09:25:11 +0900

    feat: use nord colorshceme

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,7 +2,7 @@ local wezterm = require 'wezterm';
 return {
   font = wezterm.font("UDEV Gothic NFLG"),
   font_size = 13,
-  color_scheme = "Gruvbox Dark",
+  color_scheme = "nord",
   enable_tab_bar = false,
   enable_scroll_bar = false,
   scrollback_lines = 0,
