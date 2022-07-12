22be152900918867e3297a3502a4f98847aa9416
Shinnosuke Sawada <6warashi9@gmail.com>
2022-07-12 09:31:04 +0900

    feat(wezterm): disable tabbar

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -4,11 +4,11 @@ return {
   font_size = 13,
   line_height = 1.2,
   color_scheme = "nord",
-  enable_tab_bar = true,
+  enable_tab_bar = false,
   enable_scroll_bar = false,
   scrollback_lines = 0,
   window_frame = {
-    font = wezterm.font({family="UDEV Gothic NFLG", weight="Bold"}),
+    font = wezterm.font({ family = "UDEV Gothic NFLG", weight = "Bold" }),
     font_size = 13,
   },
 }
