ba038c487a46ef394979bc3052582baf33eaae41
Shinnosuke Sawada <6warashi9@gmail.com>
2022-09-14 09:19:41 +0900

    feat: null-ls

--- .config/nixpkgs/common/packages.nix
+++ .config/nixpkgs/common/packages.nix
@@ -1,26 +1,33 @@
-{ pkgs }:
-
-let
-  warashiPkgs = import <warashi> { };
+{pkgs}: let
+  warashiPkgs = import <warashi> {};
 in
+  with pkgs; [
+    bat
+    delta
+    exa
+    fd
+    gh
+    ghq
+    git
+    git-lfs
+    glow
+    go_1_19
+    htop
+    jq
+    mosh
+    neovim
+    ripgrep
+    skim
+    tig
+    yq-go
 
-with pkgs; [
-  bat
-  delta
-  exa
-  fd
-  gh
-  ghq
-  git
-  git-lfs
-  glow
-  go_1_19
-  htop
-  jq
-  mosh
-  neovim
-  ripgrep
-  skim
-  tig
-  yq-go
-]
+    # null-ls から利用
+    alejandra
+    deadnix
+    selene
+    shellcheck
+    shellharden
+    statix
+    stylua
+    vale
+  ]
--- .config/nvim/lua/rc/null-ls.lua
+++ .config/nvim/lua/rc/null-ls.lua
@@ -1,21 +1,35 @@
-local null_ls = require('null-ls')
-null_ls.setup {
-  sources = {
-    --- spaces ---
-    null_ls.builtins.diagnostics.trail_space,
-    null_ls.builtins.formatting.trim_newlines,
-    null_ls.builtins.formatting.trim_whitespace,
+local null_ls = require("null-ls")
+null_ls.setup({
+	sources = {
+		--- dictionary / spell ---
+		null_ls.builtins.hover.dictionary,
+		null_ls.builtins.completion.spell,
+		null_ls.builtins.diagnostics.vale,
+		null_ls.builtins.diagnostics.textlint.with({
+			prefer_local = "node_modules/.bin",
+			filetypes = { "markdown", "text" },
+		}),
 
-    --- dictionary / spell ---
-    null_ls.builtins.hover.dictionary,
-    null_ls.builtins.diagnostics.misspell,
-    null_ls.builtins.diagnostics.textlint.with {
-      prefer_local = 'node_modules/.bin',
-      filetypes = { 'markdown', 'text' },
-    },
+		--- git ---
+		null_ls.builtins.diagnostics.commitlint.with({
+			prefer_local = "node_modules/.bin",
+		}),
 
-    --- fish ---
-    null_ls.builtins.diagnostics.fish,
-    null_ls.builtins.formatting.fish_indent,
-  },
-}
+		--- nix ---
+		null_ls.builtins.diagnostics.deadnix,
+		null_ls.builtins.diagnostics.statix,
+		null_ls.builtins.formatting.alejandra,
+
+		--- lua ---
+		null_ls.builtins.diagnostics.selene,
+		null_ls.builtins.formatting.stylua,
+
+		--- shell ---
+		null_ls.builtins.diagnostics.shellcheck,
+		null_ls.builtins.diagnostics.zsh,
+		null_ls.builtins.formatting.shellharden,
+
+		--- other ---
+		null_ls.builtins.diagnostics.todo_comments,
+	},
+})
