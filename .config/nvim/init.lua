1dc40658023058827276ad1e5a6a406d4e3bb6d0
Shinnosuke Sawada <6warashi9@gmail.com>
2022-09-17 20:32:31 +0900

    perf: impatient and disable-defaults

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,6 @@
+require("impatient")
+require("rc.disable-defaults")
+
 vim.o.t_8f = [[\<Esc>[38;2;%lu;%lu;%lum]] -- 文字色
 vim.o.t_8b = [[\<Esc>[48;2;%lu;%lu;%lum]] -- 背景色
 vim.g.mapleader = ","
--- .config/nvim/lua/rc/packer.lua
+++ .config/nvim/lua/rc/packer.lua
@@ -1,12 +1,16 @@
 local fn = vim.fn
 local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
 if fn.empty(fn.glob(install_path)) > 0 then
-	PackerBootstrap =
+	local PackerBootstrap =
 		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
 end
 require("packer").startup(function(use)
 	-- packer
 	use({ "wbthomason/packer.nvim" })
+
+	-- 高速化
+	use({ "lewis6991/impatient.nvim" })
+	use({ "nathom/filetype.nvim" })
 
 	-- libraries
 	use({ "nvim-lua/popup.nvim" })
