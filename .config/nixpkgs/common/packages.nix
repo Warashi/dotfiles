acc9b46af2f58ba00b644fd0dcc20df30b71ef28
Shinnosuke Sawada <6warashi9@gmail.com>
2022-09-23 12:39:51 +0900

    feat: install felix

--- .config/nixpkgs/common/packages.nix
+++ .config/nixpkgs/common/packages.nix
@@ -1,34 +1,38 @@
-{pkgs}:
-with pkgs; [
-  _1password
-  awscli2
-  bat
-  delta
-  exa
-  fd
-  gh
-  ghq
-  git
-  git-lfs
-  gitui
-  glow
-  go_1_19
-  htop
-  jq
-  mosh
-  neovim
-  ripgrep
-  skim
-  tig
-  yq-go
+{pkgs}: let
+  warashiPkgs = import <warashi> {};
+in
+  with pkgs; [
+    _1password
+    awscli2
+    bat
+    delta
+    exa
+    fd
+    gh
+    ghq
+    git
+    git-lfs
+    gitui
+    glow
+    go_1_19
+    htop
+    jq
+    mosh
+    neovim
+    ripgrep
+    skim
+    tig
+    yq-go
 
-  # null-ls から利用
-  alejandra
-  deadnix
-  selene
-  shellcheck
-  shellharden
-  statix
-  stylua
-  vale
-]
+    warashiPkgs.felix
+
+    # null-ls から利用
+    alejandra
+    deadnix
+    selene
+    shellcheck
+    shellharden
+    statix
+    stylua
+    vale
+  ]
