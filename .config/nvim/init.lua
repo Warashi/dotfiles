bd72c02f44729b3c8959bc74a08213e9f5a0680d
Shinnosuke Sawada <6warashi9@gmail.com>
2022-09-23 12:57:16 +0900

    fix: lazy-load は問題がでがちなのでやめる

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,8 +1,4 @@
 require("impatient")
 require("rc.disable-defaults")
-vim.api.nvim_create_autocmd("VimEnter", {
-	callback = function()
-		require("rc.packer")
-		require("rc.init")
-	end,
-})
+require("rc.packer")
+require("rc.init")
