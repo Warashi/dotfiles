c20f9289203064ec431640fc1e1b747710c5f929
Shinnosuke Sawada <6warashi9@gmail.com>
2022-09-26 07:45:05 +0900

    perf: add VimEnter lazy-load

--- .config/nvim/lua/rc/packer.lua
+++ .config/nvim/lua/rc/packer.lua
@@ -38,6 +38,7 @@ packer.startup(function(use)
   use({
     "rcarriga/nvim-notify",
     config = function() require("rc.notify") end,
+    event = "VimEnter",
   })
   use({ "vim-denops/denops.vim" })
 
@@ -105,6 +106,7 @@ packer.startup(function(use)
   use({
     "folke/which-key.nvim",
     config = function() require("which-key").setup({}) end,
+    event = "VimEnter",
   })
   use({
     "folke/trouble.nvim",
