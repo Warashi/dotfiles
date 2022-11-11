db12452cda71743ab6aa8bda7f79173502dac6e1
Shinnosuke Sawada <6warashi9@gmail.com>
2022-11-12 00:23:22 +0900

    fix: remove yabai overlay

--- .config/nixpkgs/common/overlays.nix
+++ .config/nixpkgs/common/overlays.nix
@@ -2,16 +2,4 @@
   # (import (builtins.fetchTarball {
   #   url = "https://github.com/nix-community/neovim-nightly-overlay/archive/master.tar.gz";
   # }))
-  (_: prev: {
-    yabai = prev.yabai.overrideAttrs (_: {
-      version = "5.0.1";
-      src = prev.fetchurl {
-        url = "https://github.com/koekeishiya/yabai/releases/download/v5.0.1/yabai-v5.0.1.tar.gz";
-        sha256 = "0czxhf95mnpycbp7aqz9ajgnb0zmazdh9lhj14gn4kpnwj64kwmd";
-      };
-      buildPhase = ''
-        echo noop
-      '';
-    });
-  })
 ]
