608a8ef641c9b5367c2f7aec0adcf1b60ea895de
Shinnosuke Sawada <6warashi9@gmail.com>
2022-11-21 12:48:00 +0900

    refactor: treesitter disable

--- .config/nvim/lua/rc/nvim-treesitter.lua
+++ .config/nvim/lua/rc/nvim-treesitter.lua
@@ -13,13 +13,11 @@ require("nvim-treesitter.configs").setup({
     enable = true,
     additional_vim_regex_highlighting = false,
     disable = function(lang)
-      local ok = true
-      ok = pcall(function() vim.treesitter.get_parser(0, lang):parse() end) and ok
-      ok = pcall(function() vim.treesitter.get_query(lang, "highlights") end) and ok
-      if not ok then return true end
-
       local byte_size = vim.api.nvim_buf_get_offset(0, vim.api.nvim_buf_line_count(0))
       if byte_size > 1024 * 1024 then return true end
+
+      if not pcall(function() vim.treesitter.get_parser(0, lang):parse() end) then return true end
+      if not pcall(function() vim.treesitter.get_query(lang, "highlights") end) then return true end
 
       return false
     end,
