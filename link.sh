65e02bc81ad1252b085f706df99588348bce9a1c
Shinnosuke Sawada <6warashi9@gmail.com>
2022-11-28 12:59:03 +0900

    style: format

--- link.sh
+++ link.sh
@@ -1,13 +1,13 @@
-#!/bin/sh
+#!/bin/bash
 cd "$(dirname "${BASH_SOURCE:-$0}")" || exit 1
 DOTPATH="$PWD"
 
 find -L "$DOTPATH" \
-  -type f \
-  -and -not -path "$DOTPATH/.git/*" \
-  -and -not -path "$DOTPATH/link.sh" \
-  -and -not -path "$DOTPATH/.nlsp-settings/*" \
-  | while read -r f; do
-  mkdir -p "$(dirname "$HOME/${f#"$DOTPATH/"}")"
-  ln -fvns "$f" "$HOME/${f#"$DOTPATH/"}"
-done
+	-type f \
+	-and -not -path "$DOTPATH/.git/*" \
+	-and -not -path "$DOTPATH/link.sh" \
+	-and -not -path "$DOTPATH/.nlsp-settings/*" |
+	while read -r f; do
+		mkdir -p "$(dirname "$HOME/${f#"$DOTPATH/"}")"
+		ln -fvns "$f" "$HOME/${f#"$DOTPATH/"}"
+	done
