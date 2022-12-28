025f7e90a4d28bb6c393614424e354f76ea19d21
Shinnosuke Sawada <6warashi9@gmail.com>
2022-12-28 13:44:43 +0900

    fix: file module の target は homedir からの相対パス

--- .config/nixpkgs/common/files.nix
+++ .config/nixpkgs/common/files.nix
@@ -1,6 +1,6 @@
 {home, ...}: {
   home.file.p10k = {
-    target = "$HOME/.config/zsh/.p10k.zsh";
+    target = ".config/zsh/.p10k.zsh";
     text = builtins.readFile ./files/p10k-config.zsh;
   };
 }
