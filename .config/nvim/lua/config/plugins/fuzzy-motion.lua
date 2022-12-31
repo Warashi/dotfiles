9e9910d68dd5767ce3f0e82247719d81e0d7e431
Shinnosuke Sawada <6warashi9@gmail.com>
2022-12-31 15:13:03 +0900

    fix: fuzzy motion lazy loading

--- .config/nvim/lua/config/plugins/fuzzy-motion.lua
+++ .config/nvim/lua/config/plugins/fuzzy-motion.lua
@@ -1,6 +1,8 @@
 local M = {
   "yuki-yano/fuzzy-motion.vim",
-  cmd = "FuzzyMotion",
+  dependencies = { "vim-denops/denops.vim" },
+  event = "User DenopsReady",
 }
 function M.init() vim.keymap.set("n", "<leader>f", "<cmd>FuzzyMotion<cr>") end
+
 return M
