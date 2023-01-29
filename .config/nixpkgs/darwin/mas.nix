5e6e79207fbd17ea399bd83f0d72a1aa0caae3c4
Shinnosuke Sawada <6warashi9@gmail.com>
2023-01-29 13:46:18 +0900

    chore: mas listを整備した

--- .config/nixpkgs/darwin/mas.nix
+++ .config/nixpkgs/darwin/mas.nix
@@ -1,19 +1,17 @@
 {
+  Bitwarden = 1352778147;
   Calcbot = 931657367;
   DaisyDisk = 411643860;
   Dictionary = 1380563956;
   Fantastical = 975937182;
   GoodLinks = 1474335294;
-  HomeControl = 1547121417;
   LINE = 539883307;
   Parcel = 639968404;
-  Pastebot = 1179623856;
-  Pocket = 568494494;
+  Paste = 967805235;
   Reeder = 1529448980;
+  Save_to_Raindropio = 1549370672;
   Tailscale = 1475387142;
   Things = 904280696;
-  TweetShot = 1227057295;
-  Tweetbot = 1384080005;
   Twitter = 1482454543;
   iStat_Menus = 1319778037;
 }
