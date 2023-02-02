379f00b58add68e66c0cc196b01c7509c89c8999
Shinnosuke Sawada <6warashi9@gmail.com>
2023-02-02 11:19:10 +0900

    refactor: change style

--- .config/nixpkgs/common/files.nix
+++ .config/nixpkgs/common/files.nix
@@ -1,44 +1,46 @@
 {home, ...}: {
-  home.file.nvredit = {
-    target = ".local/bin/nvredit";
-    source = ./. + "/files/nvredit";
-    executable = true;
-  };
-  home.file.rgfind = {
-    target = ".local/bin/rgfind";
-    source = ./. + "/files/rgfind";
-    executable = true;
-  };
-  home.file.p10k = {
-    target = ".config/zsh/.p10k.zsh";
-    source = ./. + "/files/p10k-config.zsh";
-  };
-  home.file.rg = {
-    target = ".config/ripgrep/config";
-    text = ''
-      --hidden
-      --glob=!.git/
-    '';
-  };
-  home.file.oj-template = {
-    target = ".config/online-judge-tools/template/main.go";
-    text = ''
-      package main
+  home.file = {
+    nvredit = {
+      target = ".local/bin/nvredit";
+      source = ./. + "/files/nvredit";
+      executable = true;
+    };
+    rgfind = {
+      target = ".local/bin/rgfind";
+      source = ./. + "/files/rgfind";
+      executable = true;
+    };
+    p10k = {
+      target = ".config/zsh/.p10k.zsh";
+      source = ./. + "/files/p10k-config.zsh";
+    };
+    rg = {
+      target = ".config/ripgrep/config";
+      text = ''
+        --hidden
+        --glob=!.git/
+      '';
+    };
+    oj-template = {
+      target = ".config/online-judge-tools/template/main.go";
+      text = ''
+        package main
 
-      import "github.com/Warashi/go-atcoder/lib/myio"
+        import "github.com/Warashi/go-atcoder/lib/myio"
 
-      func main() {
-      	defer myio.Flush()
+        func main() {
+        	defer myio.Flush()
 
-      }
-    '';
-  };
-  home.file.largedict = {
-    source = builtins.fetchurl "https://github.com/skk-dev/dict/raw/master/SKK-JISYO.L";
-    target = ".config/skk/SKK-JISYO.L";
-  };
-  home.file.wikidict = {
-    source = builtins.fetchurl "https://github.com/tokuhirom/skk-jisyo-jawiki/raw/master/SKK-JISYO.jawiki";
-    target = ".config/skk/SKK-JISYO.jawiki";
+        }
+      '';
+    };
+    largedict = {
+      source = builtins.fetchurl "https://github.com/skk-dev/dict/raw/master/SKK-JISYO.L";
+      target = ".config/skk/SKK-JISYO.L";
+    };
+    wikidict = {
+      source = builtins.fetchurl "https://github.com/tokuhirom/skk-jisyo-jawiki/raw/master/SKK-JISYO.jawiki";
+      target = ".config/skk/SKK-JISYO.jawiki";
+    };
   };
 }
