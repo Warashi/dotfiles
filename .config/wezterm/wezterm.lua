6506b3947ec5cd4c2195441b7bc555b2adf4ca3e
Shinnosuke Sawada <6warashi9@gmail.com>
2023-04-01 09:28:30 +0900

    feat(wezterm): catppuccin mocha

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -3,7 +3,7 @@ local wezterm = require("wezterm")
 local M = {
   hide_tab_bar_if_only_one_tab = true,
 
-  color_scheme = "Catppuccin Latte",
+  color_scheme = "Catppuccin Mocha",
 
   line_height = 1.2,
   font_size = 18.0,
