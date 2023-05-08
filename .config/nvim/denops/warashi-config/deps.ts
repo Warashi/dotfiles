76b30e61701c6accf8f5110cb29049372749d405
Shinnosuke Sawada <6warashi9@gmail.com>
2023-05-08 23:04:00 +0900

    feat(nvim): add nvim

--- .config/nvim/denops/warashi-config/deps.ts
+++ .config/nvim/denops/warashi-config/deps.ts
@@ -1,6 +1,7 @@
 export * from "https://deno.land/x/denops_std@v4.1.8/mod.ts";
 export * from "https://deno.land/x/denops_std@v4.1.8/batch/mod.ts";
 export * from "https://deno.land/x/denops_std@v4.1.8/function/mod.ts";
+export * from "https://deno.land/x/denops_std@v4.1.8/function/nvim/mod.ts";
 export * as mapping from "https://deno.land/x/denops_std@v4.1.8/mapping/mod.ts";
 export * as option from "https://deno.land/x/denops_std@v4.1.8/option/mod.ts";
 export * from "https://deno.land/x/denops_std@v4.1.8/variable/mod.ts";
