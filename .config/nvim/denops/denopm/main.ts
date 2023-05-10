d7f90f0f60b267c81083a617f597e09635ee178e
Shinnosuke Sawada <6warashi9@gmail.com>
2023-05-10 16:14:17 +0900

    feat(nvim/denopm): add methods

--- .config/nvim/denops/denopm/main.ts
+++ .config/nvim/denops/denopm/main.ts
@@ -1,47 +1,35 @@
-import { assertArray, assertString, Denops } from "./deps.ts";
-import * as toml from "https://deno.land/std@0.186.0/toml/mod.ts";
-import { z } from "https://deno.land/x/zod@v3.16.1/mod.ts";
-
-const pluginSchema = z.object({
-  github: z.string().optional(),
-  git: z.string().optional(),
-});
-
-type Plugin = z.infer<typeof pluginSchema>;
-
-function isPlugin(x: unknown): x is Plugin {
-  return pluginSchema.safeParse(x).success;
-}
-
-export async function main(denops: Denops): Promise<void> {
-  await Promise.resolve();
-
+import { assertString, Denops, fs, option } from "./deps.ts";
+export function main(denops: Denops): Promise<void> {
   denops.dispatcher = {
-    async load_toml(base: unknown, fn: unknown): Promise<void> {
-      await Promise.resolve();
-
+    async download_github(base, org, repo): Promise<void> {
       assertString(base);
-      assertString(fn);
-      const configs = toml.parse(Deno.readTextFileSync(fn));
-      const plugins = configs.plugins;
-      assertArray(plugins, isPlugin);
+      assertString(org);
+      assertString(repo);
 
-      for (const plugin of plugins) {
-        if (plugin.github === undefined && plugin.git === undefined) {
-          continue;
-        }
-        if (plugin.github !== undefined && plugin.git !== undefined) {
-          console.log("[WARN] both github and git is defined", plugin);
-          continue;
-        }
-
-        if (plugin.github !== undefined) {
-          plugin.git = `https://github.com/${plugin.github}`;
-        }
-        if (plugin.git === undefined) {
-          console.log("[ERROR] something is wrong", plugin);
-        }
+      const dst = `${base}/github.com/${org}/${repo}`;
+      if (await fs.exists(dst)) {
+        return;
       }
+
+      const url = `https://github.com/${org}/${repo}`;
+      const cmd = new Deno.Command("git", {
+        args: ["clone", "--filter=blob:none", url, dst],
+      });
+      await cmd.spawn().status;
+    },
+
+    async add_runtimepath_github(base, org, repo): Promise<void> {
+      assertString(base);
+      assertString(org);
+      assertString(repo);
+
+      option.runtimepath.set(
+        denops,
+        (await option.runtimepath.get(denops)) + "," +
+          `${base}/github.com/${org}/${repo}`,
+      );
     },
   };
+
+  return Promise.resolve();
 }
