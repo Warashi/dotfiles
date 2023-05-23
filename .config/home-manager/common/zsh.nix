95c49bf97609aaeb3e445176ed9e6a8346d53841
Shinnosuke Sawada <6warashi9@gmail.com>
2023-05-24 05:58:51 +0900

    fix: rename denovo-fzf

--- .config/home-manager/common/zsh.nix
+++ .config/home-manager/common/zsh.nix
@@ -35,8 +35,8 @@
       ZENO_GIT_TREE = "exa --tree";
 
       # denovo.zsh config
-      DENOVO_SELECTOR_FZF_USE_TMUX ="1";
-      DENOVO_SELECTOR_FZF_TMUX_OPTIONS ="-p 80%";
+      DENOVO_FZF_USE_TMUX ="1";
+      DENOVO_FZF_TMUX_OPTIONS ="-p 80%";
     };
 
     shellAliases = {
