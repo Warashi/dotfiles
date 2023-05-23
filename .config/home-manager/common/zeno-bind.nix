4a761da0f4be2d021208ea35609935a803559877
Shinnosuke Sawada <6warashi9@gmail.com>
2023-05-23 12:12:38 +0900

    feat(zsh): use denovo-selector-fzf for ghq cd

--- .config/home-manager/common/zeno-bind.nix
+++ .config/home-manager/common/zeno-bind.nix
@@ -4,6 +4,5 @@
     bindkey '^m' zeno-auto-snippet-and-accept-line
     bindkey '^i' zeno-completion
     bindkey '^r' zeno-history-selection
-    bindkey '^x' zeno-ghq-cd
   fi
 ''
--- .config/home-manager/common/zsh-binds.nix
+++ .config/home-manager/common/zsh-binds.nix
@@ -2,4 +2,5 @@
   autoload -Uz edit-command-line
   zle -N edit-command-line
   bindkey "\Ee" edit-command-line # ESC e で edit-command-line を発動させる
+  bindkey "^x" denovo-fzf-ghq
 ''
