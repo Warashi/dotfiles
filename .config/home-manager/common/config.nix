3d9bbe31d6f9c54b73eeb9fc699c495df38b7332
Shinnosuke Sawada <6warashi9@gmail.com>
2023-05-24 16:15:29 +0900

    feat: use catppuccin-latte for fzf

--- .config/home-manager/common/config.nix
+++ .config/home-manager/common/config.nix
@@ -69,8 +69,8 @@
     enable = true;
     enableZshIntegration = true;
     defaultOptions = [
-      # catppuccin-mocha
-      "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8,fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc,marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+      # "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8,fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc,marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8" # catppuccin-mocha
+      "--color=bg+:#ccd0da,bg:#eff1f5,spinner:#dc8a78,hl:#d20f39,fg:#4c4f69,header:#d20f39,info:#8839ef,pointer:#dc8a78,marker:#dc8a78,fg+:#4c4f69,prompt:#8839ef,hl+:#d20f39" # catppuccin-latte
     ];
     tmux.enableShellIntegration = true;
   };
