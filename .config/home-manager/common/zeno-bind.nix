1ef0faa455beff7b21bb085958dc3bc34e21e217
Shinnosuke Sawada <6warashi9@gmail.com>
2023-05-26 09:13:54 +0900

    feat: use denovo-abbrev

--- .config/home-manager/common/zeno-bind.nix
+++ .config/home-manager/common/zeno-bind.nix
@@ -1,7 +1,5 @@
 ''
   if [[ -n $ZENO_LOADED ]]; then
-    bindkey ' '  zeno-auto-snippet
-    bindkey '^m' zeno-auto-snippet-and-accept-line
     bindkey '^i' zeno-completion
     bindkey '^r' zeno-history-selection
   fi
--- .config/home-manager/common/zsh-binds.nix
+++ .config/home-manager/common/zsh-binds.nix
@@ -3,4 +3,6 @@
   zle -N edit-command-line
   bindkey "\Ee" edit-command-line # ESC e で edit-command-line を発動させる
   bindkey "^x" denovo-fzf-ghq-cd
+  bindkey ' '  denovo-abbrev-expand
+  bindkey '^m' denovo-abbrev-expand-and-accept-line
 ''
