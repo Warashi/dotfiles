b80d8fd8f7fe8cb6cbc8a1823d63f76e05f3c6e1
Shinnosuke Sawada <6warashi9@gmail.com>
2023-06-04 14:38:34 +0900

    feat: install tmux-mvr

--- .config/home-manager/common/packages.nix
+++ .config/home-manager/common/packages.nix
@@ -1,56 +1,60 @@
-{pkgs}:
-with pkgs; [
-  _1password
-  awscli2
-  bat
-  bc # https://github.com/NixOS/nixpkgs/issues/225579
-  delta
-  deno
-  exa
-  fd
-  gh
-  ghq
-  git
-  git-lfs
-  glow
-  gnumake
-  go_1_20
-  htop
-  hyperfine
-  jdk17
-  jq
-  kubectl
-  kubectx
-  neovim
-  neovim-remote
-  nodejs
-  okteto
-  python311
-  ripgrep
-  sheldon
-  tig
-  tmux-xpanes
-  vivid
-  yq-go
-  zstd
+{pkgs, ...}: let
+  warashiPkgs = import <warashi> {};
+in
+  with pkgs; [
+    _1password
+    awscli2
+    bat
+    bc # https://github.com/NixOS/nixpkgs/issues/225579
+    delta
+    deno
+    exa
+    fd
+    gh
+    ghq
+    git
+    git-lfs
+    glow
+    gnumake
+    go_1_20
+    htop
+    hyperfine
+    jdk17
+    jq
+    kubectl
+    kubectx
+    neovim
+    neovim-remote
+    nodejs
+    okteto
+    python311
+    ripgrep
+    sheldon
+    tig
+    tmux-xpanes
+    vivid
+    yq-go
+    zstd
 
-  python311Packages.pipx
+    warashiPkgs.tmux-mvr
 
-  # lanuage server
-  gopls
-  lua-language-server
-  nodePackages_latest.typescript-language-server
-  terraform-ls
+    python311Packages.pipx
 
-  # null-ls から利用
-  alejandra
-  beautysh
-  deadnix
-  nodePackages.cspell
-  selene
-  shellcheck
-  shellharden
-  shfmt
-  statix
-  stylua
-]
+    # lanuage server
+    gopls
+    lua-language-server
+    nodePackages_latest.typescript-language-server
+    terraform-ls
+
+    # null-ls から利用
+    alejandra
+    beautysh
+    deadnix
+    nodePackages.cspell
+    selene
+    shellcheck
+    shellharden
+    shfmt
+    statix
+    stylua
+  ]
