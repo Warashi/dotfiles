90b34ff7bdc30825d9d84d9c3762c4d9a1065f1c
Shinnosuke Sawada <6warashi9@gmail.com>
2023-06-14 06:46:41 +0900

    feat(nvim): heirline colors

--- .config/nvim/deinrc/hooks/heirline.lua
+++ .config/nvim/deinrc/hooks/heirline.lua
@@ -1,6 +1,21 @@
 -- lua_source {{{
+local palette = require("catppuccin.palettes").get_palette()
+local colors = {
+  bg = palette.base,
+  fg = palette.text,
+  red = palette.red,
+  green = palette.green,
+  yellow = palette.yellow,
+  blue = palette.blue,
+  magenta = palette.peach,
+  cyan = palette.teal,
+  dark = palette.mantle,
+}
 require("heirline").setup({
   statusline = require("config.heirline.statusline"),
   winbar = require("config.heirline.winbar"),
+  opts = {
+    colors = colors,
+  },
 })
 -- }}}
