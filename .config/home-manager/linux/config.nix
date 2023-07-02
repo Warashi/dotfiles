ee1c17a7479aee47b7f1372a027ffb72789884a8
Shinnosuke Sawada <6warashi9@gmail.com>
2023-07-02 09:59:09 +0900

    Revert "feat(linux): remove neovim systemd service"
    This reverts commit 4d758e75827e4157a7ad3501e3bddc35b83ddc6f.

--- .config/home-manager/linux/config.nix
+++ .config/home-manager/linux/config.nix
@@ -7,6 +7,7 @@
 in {
   imports = [
     ../common/config.nix
+    ./systemd.nix
   ];
 
   home.username = local.user;
---
+++ .config/home-manager/linux/systemd.nix
@@ -0,0 +1,19 @@
+{
+  systemd,
+  pkgs,
+  ...
+}: {
+  systemd.user.services = {
+    neovim = {
+      Unit = {
+        Description = "neovim text editor";
+        Documentation = "man:nvim(1)";
+      };
+      Service = {
+        ExecStart = ''${pkgs.zsh}/bin/zsh -c ". /etc/zshrc; exec ${pkgs.neovim}/bin/nvim --headless --listen ${builtins.getEnv "XDG_RUNTIME_DIR"}/nvim.socket"'';
+        ExecStop = ''${pkgs.neovim}/bin/nvim --server ${builtins.getEnv "XDG_RUNTIME_DIR"}/nvim.socket --remote-send "<C-\><C-N>:wqa<CR>"'';
+        Restart = ''always'';
+      };
+    };
+  };
+}
