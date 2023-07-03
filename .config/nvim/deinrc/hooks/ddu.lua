47efd7fe42d89bbf24cd8af80236ba4ba60ab471
Shinnosuke Sawada <6warashi9@gmail.com>
2023-07-03 10:27:19 +0900

    fix(nvim): move keymap configs to lua_add

--- .config/nvim/deinrc/hooks/ddu.lua
+++ .config/nvim/deinrc/hooks/ddu.lua
@@ -13,6 +13,38 @@ local function set_keymaps()
       })
     end
   )
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "ddu-ff",
+    callback = function()
+      local action = vim.fn["ddu#ui#ff#do_action"]
+      local opts = { buffer = true }
+      vim.keymap.set("n", "<CR>", function() action("itemAction") end, opts)
+      vim.keymap.set("n", "<Space>", function() action("toggleSelectItem") end, opts)
+      vim.keymap.set("n", "i", function() action("openFilterWindow") end, opts)
+      vim.keymap.set("n", "q", function() action("quit") end, opts)
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("FileType", {
+    pattern = "ddu-ff-filter",
+    callback = function()
+      local opts = { buffer = true }
+      vim.keymap.set("i", "<CR>", "<Esc><Cmd>call ddu#ui#do_action('closeFilterWindow')<CR>", opts)
+      vim.keymap.set("n", "<CR>", "<Cmd>call ddu#ui#do_action('closeFilterWindow')<CR>", opts)
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("LspAttach_ddu", {}),
+    callback = function(ev)
+      local opts = { buffer = ev.buf }
+      vim.keymap.set("n", "gd", function() start({ name = "lsp-definition" }) end, opts)
+      vim.keymap.set("n", "gr", function() start({ name = "lsp-references" }) end, opts)
+      vim.keymap.set("n", "<space>q", function() start({ name = "lsp-diagnostic" }) end, opts)
+      vim.keymap.set({ "n", "v" }, "<space>ca", function() start({ name = "lsp-codeAction" }) end, opts)
+    end,
+  })
 end
 
 set_keymaps()
@@ -20,37 +52,3 @@ set_keymaps()
 
 -- lua_source {{{
 vim.fn["ddu#custom#load_config"](vim.env.DEIN_CONFIG_BASE .. "hooks/ddu.ts")
-
--- key-bindings
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "ddu-ff",
-  callback = function()
-    local action = vim.fn["ddu#ui#ff#do_action"]
-    local opts = { buffer = true }
-    vim.keymap.set("n", "<CR>", function() action("itemAction") end, opts)
-    vim.keymap.set("n", "<Space>", function() action("toggleSelectItem") end, opts)
-    vim.keymap.set("n", "i", function() action("openFilterWindow") end, opts)
-    vim.keymap.set("n", "q", function() action("quit") end, opts)
-  end,
-})
-
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = "ddu-ff-filter",
-  callback = function()
-    local opts = { buffer = true }
-    vim.keymap.set("i", "<CR>", "<Esc><Cmd>call ddu#ui#do_action('closeFilterWindow')<CR>", opts)
-    vim.keymap.set("n", "<CR>", "<Cmd>call ddu#ui#do_action('closeFilterWindow')<CR>", opts)
-  end,
-})
-
-vim.api.nvim_create_autocmd("LspAttach", {
-  group = vim.api.nvim_create_augroup("LspAttach_ddu", {}),
-  callback = function(ev)
-    local start = vim.fn["ddu#start"]
-    local opts = { buffer = ev.buf }
-    vim.keymap.set("n", "gd", function() start({ name = "lsp-definition" }) end, opts)
-    vim.keymap.set("n", "gr", function() start({ name = "lsp-references" }) end, opts)
-    vim.keymap.set("n", "<space>q", function() start({ name = "lsp-diagnostic" }) end, opts)
-    vim.keymap.set({ "n", "v" }, "<space>ca", function() start({ name = "lsp-codeAction" }) end, opts)
-  end,
-})
