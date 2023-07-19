4d6a5d2f0c947ef27b5b9e39734b34611198099c
Shinnosuke Sawada <6warashi9@gmail.com>
2023-07-20 07:27:57 +0900

    feat(wezterm): パスワード入力のPoC

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,16 +1,52 @@
 local wezterm = require("wezterm")
+local act = wezterm.action
+local config = wezterm.config_builder()
 
-local M = {
-  hide_tab_bar_if_only_one_tab = true,
+wezterm.on("trigger-password-input", function(window, pane)
+  local choices = {
+    { label = "local" },
+    { label = "remote" },
+  }
+  window:perform_action(
+    act.InputSelector({
+      action = wezterm.action_callback(function(window, pane, id, label)
+        if not id and not label then
+          wezterm.log_info("cancelled")
+        else
+          local _, stdout, _ = wezterm.run_child_process({
+            "security",
+            "find-generic-password",
+            "-w",
+            "-a",
+            label,
+            "-s",
+            "wezterm-password",
+          })
+          pane:send_text(stdout)
+        end
+      end),
+      title = "select account",
+      choices = choices,
+    }),
+    pane
+  )
+end)
 
-  color_scheme = "Catppuccin Latte",
-
-  line_height = 1.2,
-  font_size = 18.0,
-  font = wezterm.font_with_fallback({
-    { family = "UDEV Gothic NFLG" },
-    { family = "UDEV Gothic NFLG", assume_emoji_presentation = true },
-  }),
+config.keys = {
+  {
+    key = "p",
+    mods = "CTRL|SHIFT|META",
+    action = act.EmitEvent("trigger-password-input"),
+  },
 }
 
-return M
+config.hide_tab_bar_if_only_one_tab = true
+config.color_scheme = "Catppuccin Latte"
+config.line_height = 1.2
+config.font_size = 18.0
+config.font = wezterm.font_with_fallback({
+  { family = "UDEV Gothic NFLG" },
+  { family = "UDEV Gothic NFLG", assume_emoji_presentation = true },
+})
+
+return config
