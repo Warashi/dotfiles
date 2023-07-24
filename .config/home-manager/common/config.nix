68bdda1df6f0bc1cc6fc7ec4761b835a8a88575f
Shinnosuke Sawada <6warashi9@gmail.com>
2023-07-24 09:49:50 +0900

    feat(starship): use catppuccin_latte

--- .config/home-manager/common/config.nix
+++ .config/home-manager/common/config.nix
@@ -83,5 +83,18 @@
 
   programs.starship = {
     enable = true;
+    settings =
+      {
+        palette = "catppuccin_latte";
+      }
+      // builtins.fromTOML (builtins.readFile
+        (pkgs.fetchFromGitHub
+          {
+            owner = "catppuccin";
+            repo = "starship";
+            rev = "5629d2356f62a9f2f8efad3ff37476c19969bd4f";
+            sha256 = "sha256-nsRuxQFKbQkyEI4TXgvAjcroVdG+heKX5Pauq/4Ota0=";
+          }
+          + /palettes/latte.toml));
   };
 }
