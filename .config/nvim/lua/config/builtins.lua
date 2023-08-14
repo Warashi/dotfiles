cacc41d9b68325c7b61d51731c5ca6ff573cf90c
Shinnosuke Sawada <6warashi9@gmail.com>
2023-08-14 16:37:19 +0900

    feat(nvim): signcolumnを常に表示するように変更

--- .config/nvim/lua/config/builtins.lua
+++ .config/nvim/lua/config/builtins.lua
@@ -7,6 +7,7 @@ vim.o.expandtab = true
 vim.o.tabstop = 2
 vim.o.shiftwidth = 2
 vim.o.clipboard = "unnamedplus"
+vim.o.signcolumn = "yes"
 vim.o.showmode = false
 vim.o.cmdheight = 0
 vim.o.laststatus = 3
