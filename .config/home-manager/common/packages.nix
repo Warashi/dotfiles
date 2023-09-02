69ee5a62bf199bcd4625da035bff3a8d39d056d6
Shinnosuke Sawada <6warashi9@gmail.com>
2023-09-02 17:07:39 +0900

    feat: gui持ってるかどうかで入れるpackageを分けた

--- .config/home-manager/common/packages.nix
+++ .config/home-manager/common/packages.nix
@@ -1,64 +1,73 @@
-{pkgs, ...}:
-with pkgs; [
-  _1password
-  alacritty
-  awscli2
-  bat
-  delta
-  deno
-  discord
-  exa
-  fd
-  gh
-  ghq
-  git
-  git-lfs
-  glow
-  gnumake
-  go_1_21
-  htop
-  hyperfine
-  jdk17
-  jq
-  kubectl
-  kubectx
-  mocword
-  mocword-data
-  neovim
-  neovim-remote
-  nodejs
-  okteto
-  python311
-  ripgrep
-  sheldon
-  slack
-  taplo
-  tig
-  tmux-mvr
-  tmux-xpanes
-  vivid
-  wezterm
-  yq-go
-  zstd
+{pkgs, ...}: let
+  local = import ../local.nix;
+in
+  with pkgs;
+    [
+      _1password
+      awscli2
+      bat
+      delta
+      deno
+      exa
+      fd
+      gh
+      ghq
+      git
+      git-lfs
+      glow
+      gnumake
+      go_1_21
+      htop
+      hyperfine
+      jdk17
+      jq
+      kubectl
+      kubectx
+      mocword
+      mocword-data
+      neovim
+      neovim-remote
+      nodejs
+      okteto
+      python311
+      ripgrep
+      sheldon
+      taplo
+      tig
+      tmux-mvr
+      tmux-xpanes
+      vivid
+      wezterm
+      yq-go
+      zstd
 
-  python311Packages.pipx
+      python311Packages.pipx
 
-  # lanuage server
-  buf-language-server
-  lua-language-server
-  nodePackages_latest.typescript-language-server
-  nodePackages_latest.yaml-language-server
-  terraform-ls
+      # lanuage server
+      buf-language-server
+      lua-language-server
+      nodePackages_latest.typescript-language-server
+      nodePackages_latest.yaml-language-server
+      terraform-ls
 
-  # null-ls から利用
-  alejandra
-  beautysh
-  deadnix
-  nodePackages.cspell
-  selene
-  shellcheck
-  shellharden
-  shfmt
-  statix
-  stylua
-]
+      # null-ls から利用
+      alejandra
+      beautysh
+      deadnix
+      nodePackages.cspell
+      selene
+      shellcheck
+      shellharden
+      shfmt
+      statix
+      stylua
+    ]
+    ++ (
+      if local.hasGUI
+      then [
+        alacritty
+        discord
+        slack
+      ]
+      else []
+    )
