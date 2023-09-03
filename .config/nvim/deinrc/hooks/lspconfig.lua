7e154b88f036a4e7ea1e58bad11b9cb436273fc2
Shinnosuke Sawada <6warashi9@gmail.com>
2023-09-03 17:39:45 +0900

    feat(nvim): inlayhintに合わせていろいろ調整

--- .config/nvim/deinrc/hooks/lspconfig.lua
+++ .config/nvim/deinrc/hooks/lspconfig.lua
@@ -7,9 +7,13 @@ vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 vim.api.nvim_create_autocmd("LspAttach", {
   group = vim.api.nvim_create_augroup("UserLspConfig", {}),
   callback = function(ev)
-    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+    local bufnr = ev.buf
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+    if client == nil then return end
 
-    local opts = { buffer = ev.buf }
+    vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+
+    local opts = { buffer = bufnr }
     vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
     vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
     vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
@@ -17,7 +21,8 @@ vim.api.nvim_create_autocmd("LspAttach", {
     vim.keymap.set("n", "<space>wl", function() print(vim.inspect(vim.lsp.buf.list_workspace_folders())) end, opts)
     vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
     vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-    vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format({ async = true }) end, opts)
+    vim.keymap.set("n", "<space>f", function() vim.lsp.buf.format({ async = false }) end, opts)
+    vim.keymap.set("n", "<space>i", function() vim.lsp.inlay_hint(bufnr, nil) end, opts)
   end,
 })
 -- }}}
@@ -25,10 +30,10 @@ vim.api.nvim_create_autocmd("LspAttach", {
 -- lua_source {{{
 require("ddc_nvim_lsp_setup").setup({})
 local lspconfig = require("lspconfig")
+local util = require("lspconfig.util")
 
 for _, server in ipairs({
   "bufls",
-  "lua_ls",
   "rust_analyzer",
   "taplo",
   "terraformls",
@@ -38,8 +43,32 @@ for _, server in ipairs({
   lspconfig[server].setup({})
 end
 
-lspconfig.denols.setup({
-  root_dir = lspconfig.util.root_pattern("deno.json", "deno.jsonc"),
+lspconfig["lua_ls"].setup({
+  settings = {
+    Lua = {
+      runtime = {
+        version = "LuaJIT",
+      },
+      diagnostics = {
+        globals = { "vim" },
+      },
+      format = {
+        enable = false,
+      },
+      workspace = {
+        library = vim.api.nvim_get_runtime_file("", true),
+        checkThirdParty = false,
+      },
+      hint = {
+        enable = true,
+        setType = true,
+      },
+    },
+  },
+})
+
+lspconfig["denols"].setup({
+  root_dir = util.root_pattern("deno.json", "deno.jsonc"),
   init_options = {
     lint = true,
     unstable = true,
@@ -52,15 +81,57 @@ lspconfig.denols.setup({
         },
       },
     },
+    inlayHints = {
+      parameterNames = {
+        enabled = "all",
+      },
+      parameterTypes = {
+        enabled = true,
+      },
+      variableTypes = {
+        enabled = true,
+      },
+      propertyDeclarationTypes = {
+        enabled = true,
+      },
+      functionLikeReturnTypes = {
+        enabled = true,
+      },
+      enumMemberValues = {
+        enabled = true,
+      },
+    },
   },
 })
 
-lspconfig.tsserver.setup({
-  root_dir = lspconfig.util.root_pattern("package.json"),
+lspconfig["tsserver"].setup({
+  root_dir = util.root_pattern("package.json"),
   single_file_support = false,
+  typescript = {
+    inlayHints = {
+      parameterNames = {
+        enabled = "all",
+      },
+      parameterTypes = {
+        enabled = true,
+      },
+      variableTypes = {
+        enabled = true,
+      },
+      propertyDeclarationTypes = {
+        enabled = true,
+      },
+      functionLikeReturnTypes = {
+        enabled = true,
+      },
+      enumMemberValues = {
+        enabled = true,
+      },
+    },
+  },
 })
 
-lspconfig.gopls.setup({
+lspconfig["gopls"].setup({
   settings = {
     gopls = {
       buildFlags = { "-tags=wireinject" },
