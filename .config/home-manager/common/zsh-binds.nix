55b7e20686f157ffe092aec83794c3c5fb9700a6
Shinnosuke Sawada <6warashi9@gmail.com>
2023-09-16 11:21:08 +0900

    refactor(zsh): denovoのbindkeyをsheldonに移した

--- .config/home-manager/common/zsh-binds.nix
+++ .config/home-manager/common/zsh-binds.nix
@@ -2,8 +2,4 @@
   autoload -Uz edit-command-line
   zle -N edit-command-line
   bindkey "\Ee" edit-command-line # ESC e で edit-command-line を発動させる
-  bindkey "^x" denovo-fzf-ghq-cd
-  bindkey ' '  denovo-abbrev-expand
-  bindkey '^m' denovo-abbrev-expand-and-accept-line
-  bindkey '^i' denovo-complete
 ''
