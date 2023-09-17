fff63fdc1df061a8c829460bbe7e9bb362eaacdc
Shinnosuke Sawada <6warashi9@gmail.com>
2023-09-17 16:05:03 +0900

    feat(zsh): ensure_zcompiledで作成されたzwcを全部消す方法を用意

--- .config/home-manager/common/zsh/ensure-zcompiled.nix
+++ .config/home-manager/common/zsh/ensure-zcompiled.nix
@@ -5,6 +5,7 @@
   }
   function ensure_zcompiled {
     local compiled="$1.zwc"
+    # rm -f $compiled
     if [[ ! -r "$compiled" || "$1" -nt "$compiled" ]]; then
       echo "Compiling $1"
       zcompile -R $1
