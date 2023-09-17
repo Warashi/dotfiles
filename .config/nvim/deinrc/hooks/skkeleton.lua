745e956b8b5aa73abca66f6fbade4be940fa127c
Shinnosuke Sawada <6warashi9@gmail.com>
2023-09-17 16:48:02 +0900

    feat(nvim): install skkeleton_indicator.nvim

--- .config/nvim/deinrc/hooks/skkeleton.lua
+++ .config/nvim/deinrc/hooks/skkeleton.lua
@@ -13,5 +13,4 @@ local function skkeleton_init()
 end
 vim.keymap.set({ "i", "c", "t" }, "<C-j>", "<Plug>(skkeleton-enable)", { silent = true })
 vim.api.nvim_create_autocmd("User", { pattern = "skkeleton-initialize-pre", callback = skkeleton_init })
-vim.api.nvim_create_autocmd("User", { pattern = "DenopsPluginPost:skkeleton", command = "call skkeleton#initialize()" })
 -- }}}
