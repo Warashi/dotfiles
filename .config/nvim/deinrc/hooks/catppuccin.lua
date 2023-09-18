44b7b260fd744cf3fe9df9a4a15608f351fcb3db
Shinnosuke Sawada <6warashi9@gmail.com>
2023-09-18 17:38:20 +0900

    perf(nvim): load catppuccin when colorschmed

--- .config/nvim/deinrc/hooks/catppuccin.lua
+++ .config/nvim/deinrc/hooks/catppuccin.lua
@@ -1,21 +1,25 @@
--- lua_source {{{
-require("catppuccin").setup({
-  transparent_background = false,
-  term_colors = true,
-  dim_inactive = {
-    enabled = true,
-  },
-  integrations = {
-    fern = true,
-    fidget = true,
-    gitsigns = true,
-    native_lsp = { enabled = true },
-    navic = { enabled = true },
-    notify = true,
-    semantic_tokens = true,
-    treesitter_context = true,
-    treesitter = true,
-    sandwich = true,
-  },
+-- lua_add {{{
+vim.api.nvim_create_autocmd("ColorSchemePre", {
+  pattern = "catppuccin-latte",
+  callback = function()
+    require("catppuccin").setup({
+      transparent_background = false,
+      term_colors = true,
+      dim_inactive = {
+        enabled = true,
+      },
+      integrations = {
+        fidget = true,
+        gitsigns = true,
+        native_lsp = { enabled = true },
+        navic = { enabled = true },
+        notify = true,
+        semantic_tokens = true,
+        treesitter_context = true,
+        treesitter = true,
+        sandwich = true,
+      },
+    })
+  end,
 })
 -- }}}
