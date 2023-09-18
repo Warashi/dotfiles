77dd18906f487a8f742d1d5cac4f0f2cb3e81519
Shinnosuke Sawada <6warashi9@gmail.com>
2023-09-18 14:08:34 +0900

    build(deps): use root nixpkgs in nvim-nightly-overlay

--- .config/home-manager/flake.nix
+++ .config/home-manager/flake.nix
@@ -8,7 +8,10 @@
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
+    neovim-nightly-overlay = {
+      url = "github:nix-community/neovim-nightly-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     catppuccin-alacritty = {
       url = "github:catppuccin/alacritty";
       flake = false;
