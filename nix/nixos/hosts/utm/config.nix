f6b86e22e9689670647da52082ce5246c92e4534
Shinnosuke Sawada <6warashi9@gmail.com>
2023-09-26 20:10:25 +0900

    refactor(nixos/utm): refactored

--- nix/nixos/hosts/utm/config.nix
+++ nix/nixos/hosts/utm/config.nix
@@ -3,14 +3,17 @@ _: {
     ./hardware-configuration.nix
   ];
 
-  boot.initrd.availableKernelModules = [ "virtiofs" ];
-
   # we cannot use this with networking.wireless, so this is host config.
   networking.networkmanager.enable = true;
 
-  # Bootloader.
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
+  boot = {
+    initrd.availableKernelModules = ["virtiofs"];
+    # Bootloader.
+    loader = {
+      systemd-boot.enable = true;
+      efi.canTouchEfiVariables = true;
+    };
+  };
 
   networking.hostName = "nixos"; # Define your hostname.
 }
