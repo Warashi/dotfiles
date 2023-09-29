a17b340fc5a1c43363a84193ffed0597bb69cfd1
Shinnosuke Sawada <6warashi9@gmail.com>
2023-09-30 08:37:25 +0900

    feat(xremap): Karabiner-Elementsと同じキーマップを実現

--- nix/home-manager/linux-gui/xremap.nix
+++ nix/home-manager/linux-gui/xremap.nix
@@ -4,9 +4,41 @@ _: {
     config = {
       modmap = [
         {
-          name = "CapsLock to Ctrl_L";
+          name = "CapsLock to Ctrl_R and Escape";
           remap = {
-            CapsLock = "Ctrl_L";
+            CapsLock = {
+              held = "Ctrl_R";
+              alone = "Esc";
+            };
+          };
+        }
+        {
+          name = "SandS";
+          remap = {
+            Space = {
+              held = "Shift_R";
+              alone = "Space";
+            };
+          };
+        }
+        {
+          name = "Tab to meh(Shift+Control+Alt) and Tab";
+          remap = {
+            Tab = {
+              held = ["Shift_R" "Ctrl_R" "Alt_R"];
+              alone = "Tab";
+            };
+          };
+        }
+      ];
+      keymap = [
+        {
+          name = "Shift_L + h/j/k/l to Arrows";
+          remap = {
+            Shift_L-h = "left";
+            Shift_L-j = "down";
+            Shift_L-k = "up";
+            Shift_L-l = "right";
           };
         }
       ];
