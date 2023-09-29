2c7590964c4995f49fbfcb5678ca7a79f697134e
Shinnosuke Sawada <6warashi9@gmail.com>
2023-09-29 09:21:42 +0900

    refactor(home-manager): enable xdg

--- nix/home-manager/common/files.nix
+++ nix/home-manager/common/files.nix
@@ -5,10 +5,6 @@
   ...
 }: {
   home.file = {
-    cspel = {
-      target = ".config/cspell/cspell.json";
-      source = ./. + "/files/cspell.json";
-    };
     nvredit = {
       target = ".local/bin/nvredit";
       source = ./. + "/files/nvredit";
@@ -19,42 +15,51 @@
       source = ./. + "/files/rgfind";
       executable = true;
     };
-    oj-template = {
-      target = ".config/online-judge-tools/template/main.go";
-      text = ''
-        package main
-
-        import "github.com/Warashi/go-atcoder/lib/myio"
-
-        func main() {
-        	defer myio.Flush()
-
-        }
-      '';
-    };
-    catppuccin-bat = {
-      source = inputs.catppuccin-bat;
-      target = ".config/bat/themes";
-      recursive = true;
-    };
-    catppuccin-zsh-fast-syntax-highligiting = {
-      source = inputs.catppuccin-zsh-fsh + /themes;
-      target = ".config/fsh";
-      recursive = true;
-    };
-    catppuccin-glamour-latte = {
-      source = pkgs.fetchurl {
-        url = "https://github.com/catppuccin/glamour/releases/download/v1.0.0/latte.json";
-        sha256 = "sha256-V0LsRStF1vL+Tz8G6VaKiwiY/ZIsSkMc+f1WJAITYXU=";
+  };
+  xdg = {
+    enable = true;
+    configFile = {
+      cspel = {
+        target = "cspell/cspell.json";
+        source = ./. + "/files/cspell.json";
       };
-      target = ".config/glamour/catppuccin-latte.json";
-    };
-    catppuccin-glamour-frappe = {
-      source = pkgs.fetchurl {
-        url = "https://github.com/catppuccin/glamour/releases/download/v1.0.0/frappe.json";
-        sha256 = "sha256-YOatgYCJKuesVERHZVmF1xtzuLjyxCYstoWYqATq+NU=";
+      oj-template = {
+        target = "online-judge-tools/template/main.go";
+        text = ''
+          package main
+
+          import "github.com/Warashi/go-atcoder/lib/myio"
+
+          func main() {
+          	defer myio.Flush()
+
+          }
+        '';
       };
-      target = ".config/glamour/catppuccin-frappe.json";
+      catppuccin-bat = {
+        source = inputs.catppuccin-bat;
+        target = "bat/themes";
+        recursive = true;
+      };
+      catppuccin-zsh-fast-syntax-highligiting = {
+        source = inputs.catppuccin-zsh-fsh + /themes;
+        target = "fsh";
+        recursive = true;
+      };
+      catppuccin-glamour-latte = {
+        source = pkgs.fetchurl {
+          url = "https://github.com/catppuccin/glamour/releases/download/v1.0.0/latte.json";
+          sha256 = "sha256-V0LsRStF1vL+Tz8G6VaKiwiY/ZIsSkMc+f1WJAITYXU=";
+        };
+        target = "glamour/catppuccin-latte.json";
+      };
+      catppuccin-glamour-frappe = {
+        source = pkgs.fetchurl {
+          url = "https://github.com/catppuccin/glamour/releases/download/v1.0.0/frappe.json";
+          sha256 = "sha256-YOatgYCJKuesVERHZVmF1xtzuLjyxCYstoWYqATq+NU=";
+        };
+        target = "glamour/catppuccin-frappe.json";
+      };
     };
   };
 }
