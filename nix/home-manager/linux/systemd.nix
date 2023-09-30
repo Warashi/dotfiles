e5f4a90e3475ccff3c0535915a63d52cb6d13c4b
Shinnosuke Sawada <6warashi9@gmail.com>
2023-09-30 19:33:10 +0900

    feat(linux): yaskkserv2を自動起動するようにした

--- nix/home-manager/linux/systemd.nix
+++ nix/home-manager/linux/systemd.nix
@@ -1,6 +1,5 @@
 {
   inputs,
-  systemd,
   pkgs,
   ...
 }: let
@@ -28,6 +27,9 @@ in {
       Service = {
         ExecStart = ''${pkgs.yaskkserv2}/bin/yaskkserv2 --no-daemonize --listen-address=127.0.0.1 --google-japanese-input=disable ${yaskkserv2-dictionary}'';
         Restart = ''always'';
+      };
+      Install = {
+        WantedBy = ["multi-user.target"];
       };
     };
   };
