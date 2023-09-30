6c0fe016dc827618bd77f8429e1303b37cd5a9c8
Shinnosuke Sawada <6warashi9@gmail.com>
2023-09-30 11:08:26 +0900

    feat(linux-gui): add cjk fonts

--- nix/home-manager/linux-gui/fonts.nix
+++ nix/home-manager/linux-gui/fonts.nix
@@ -18,5 +18,11 @@
   };
 in {
   fonts.fontconfig.enable = true;
-  home.packages = [plemoljp];
+  home.packages = with pkgs; [
+    plemoljp
+
+    noto-fonts-cjk-sans
+    noto-fonts-cjk-serif
+    noto-fonts-emoji
+  ];
 }
