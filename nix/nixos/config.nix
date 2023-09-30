9b77734dad513d99c3f010283fcd2726cf8891cf
Shinnosuke Sawada <6warashi9@gmail.com>
2023-09-30 19:21:39 +0900

    feat(linux-gui): use gnome-keyring and polkit_gnome

--- nix/nixos/config.nix
+++ nix/nixos/config.nix
@@ -2,8 +2,10 @@ _: {
   imports = [
     ./1password.nix
     ./display.nix
+    ./keyring.nix
     ./locale.nix
     ./nixconfig.nix
+    ./polkit.nix
     ./security.nix
     ./sound.nix
     ./tailscale.nix
