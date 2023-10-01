83690cf29c68fe0fe2b7df733c041c6ac347571e
Shinnosuke Sawada <6warashi9@gmail.com>
2023-10-01 20:05:27 +0900

    feat(linux-gui): set wallpaper and transparency

--- nix/home-manager/linux-gui/xmonad/default.nix
+++ nix/home-manager/linux-gui/xmonad/default.nix
@@ -12,6 +12,9 @@
   ];
   xsession = {
     enable = true;
+    initExtra = ''
+      ${pkgs.feh}/bin/feh --bg-scale ~/Pictures/IMG_0045.png
+    '';
     windowManager = {
       xmonad = {
         enable = true;
@@ -20,160 +23,168 @@
       };
     };
   };
-  services.polybar = {
-    enable = true;
-    script = "";
-    settings = {
-      "global/wm" = {
-        include-file = inputs.catppuccin-polybar + "/themes/frappe.ini";
-      };
+  services = {
+    picom = {
+      enable = true;
+      activeOpacity = 0.9;
+      inactiveOpacity = 0.9;
+      menuOpacity = 0.9;
+    };
+    polybar = {
+      enable = true;
+      script = "";
+      settings = {
+        "global/wm" = {
+          include-file = inputs.catppuccin-polybar + "/themes/frappe.ini";
+        };
 
-      "bar/warashi" = {
-        width = "100%";
-        height = "24pt";
-        radius = 6;
+        "bar/warashi" = {
+          width = "100%";
+          height = "24pt";
+          radius = 6;
 
-        background = ''''${colors.base}'';
-        foreground = ''''${colors.text}'';
+          background = ''''${colors.base}'';
+          foreground = ''''${colors.text}'';
 
-        line-size = "3pt";
-        border-size = "0pt";
-        border-color = ''''${colors.blue}'';
+          line-size = "3pt";
+          border-size = "0pt";
+          border-color = ''''${colors.blue}'';
 
-        padding-left = 0;
-        padding-right = 1;
+          padding-left = 0;
+          padding-right = 1;
 
-        module-margin = 1;
+          module-margin = 1;
 
-        separator = " | ";
-        separator-foreground = ''''${colors.subtext0}'';
+          separator = " | ";
+          separator-foreground = ''''${colors.subtext0}'';
 
-        font-0 = "IBM Plex Sans JP";
-        font-1 = "PlemolJP Console NF";
-        font-2 = "Noto Color Emoji";
-        font-3 = "Font Awesome 6 Free";
-        font-4 = "monospace;2";
+          font-0 = "IBM Plex Sans JP";
+          font-1 = "PlemolJP Console NF";
+          font-2 = "Noto Color Emoji";
+          font-3 = "Font Awesome 6 Free";
+          font-4 = "monospace;2";
 
-        modules-left = "xworkspaces";
-        modules-center = "xwindow";
-        modules-right = "filesystem xkeyboard memory cpu wlan eth date";
+          modules-left = "xworkspaces";
+          modules-center = "xwindow";
+          modules-right = "filesystem xkeyboard memory cpu wlan eth date";
 
-        cursor-click = "pointer";
-        cursor-scroll = "ns-resize";
+          cursor-click = "pointer";
+          cursor-scroll = "ns-resize";
 
-        enable-ipc = true;
+          enable-ipc = true;
 
-        tray-position = "right";
+          tray-position = "right";
 
-        wm-restack = "generic";
-      };
+          wm-restack = "generic";
+        };
 
-      "module/xworkspaces" = {
-        type = "internal/xworkspaces";
+        "module/xworkspaces" = {
+          type = "internal/xworkspaces";
 
-        label-active = "%name%";
-        label-active-background = ''''${colors.mantle}'';
-        label-active-underline = ''''${colors.blue}'';
-        label-active-padding = 1;
+          label-active = "%name%";
+          label-active-background = ''''${colors.mantle}'';
+          label-active-underline = ''''${colors.blue}'';
+          label-active-padding = 1;
 
-        label-occupied = "%name%";
-        label-occupied-padding = 1;
+          label-occupied = "%name%";
+          label-occupied-padding = 1;
 
-        label-urgent = "%name%";
-        label-urgent-background = ''''${colors.crust}'';
-        label-urgent-padding = 1;
+          label-urgent = "%name%";
+          label-urgent-background = ''''${colors.crust}'';
+          label-urgent-padding = 1;
 
-        label-empty = "%name%";
-        label-empty-foreground = ''''${colors.text}'';
-        label-empty-padding = 1;
-      };
+          label-empty = "%name%";
+          label-empty-foreground = ''''${colors.text}'';
+          label-empty-padding = 1;
+        };
 
-      "module/xwindow" = {
-        type = "internal/xwindow";
-        label = "%title:0:60:...%";
-      };
+        "module/xwindow" = {
+          type = "internal/xwindow";
+          label = "%title:0:60:...%";
+        };
 
-      "module/filesystem" = {
-        type = "internal/fs";
-        interval = 25;
+        "module/filesystem" = {
+          type = "internal/fs";
+          interval = 25;
 
-        mount-0 = "/";
+          mount-0 = "/";
 
-        label-mounted = "%{F#F0C674}%mountpoint%%{F-} %percentage_used%%";
+          label-mounted = "%{F#F0C674}%mountpoint%%{F-} %percentage_used%%";
 
-        label-unmounted = "%mountpoint% not mounted";
-        label-unmounted-foreground = ''''${colors.subtext0}'';
-      };
+          label-unmounted = "%mountpoint% not mounted";
+          label-unmounted-foreground = ''''${colors.subtext0}'';
+        };
 
-      "module/xkeyboard" = {
-        type = "internal/xkeyboard";
-        blacklist-0 = "num lock";
+        "module/xkeyboard" = {
+          type = "internal/xkeyboard";
+          blacklist-0 = "num lock";
 
-        label-layout = "%layout%";
-        label-layout-foreground = ''''${colors.text}'';
+          label-layout = "%layout%";
+          label-layout-foreground = ''''${colors.text}'';
 
-        label-indicator-padding = 2;
-        label-indicator-margin = 1;
-        label-indicator-foreground = ''''${colors.text}'';
-        label-indicator-background = ''''${colors.blue}'';
-      };
+          label-indicator-padding = 2;
+          label-indicator-margin = 1;
+          label-indicator-foreground = ''''${colors.text}'';
+          label-indicator-background = ''''${colors.blue}'';
+        };
 
-      "module/memory" = {
-        type = "internal/memory";
-        interval = 2;
+        "module/memory" = {
+          type = "internal/memory";
+          interval = 2;
 
-        format-prefix = "RAM ";
-        format-prefix-foreground = ''''${colors.text}'';
+          format-prefix = "RAM ";
+          format-prefix-foreground = ''''${colors.text}'';
 
-        label = "%percentage_used:2%%";
-      };
+          label = "%percentage_used:2%%";
+        };
 
-      "module/cpu" = {
-        type = "internal/cpu";
-        interval = 2;
+        "module/cpu" = {
+          type = "internal/cpu";
+          interval = 2;
 
-        format-prefix = "CPU ";
-        format-prefix-foreground = ''''${colors.text}'';
+          format-prefix = "CPU ";
+          format-prefix-foreground = ''''${colors.text}'';
 
-        label = "%percentage:2%%";
-      };
+          label = "%percentage:2%%";
+        };
 
-      "network-base" = {
-        type = "internal/network";
-        interval = 5;
+        "network-base" = {
+          type = "internal/network";
+          interval = 5;
 
-        format-connected = "<label-connected>";
-        format-disconnected = "<label-disconnected>";
+          format-connected = "<label-connected>";
+          format-disconnected = "<label-disconnected>";
 
-        label-disconnected = "%{F#F0C674}%ifname%%{F#707880} disconnected";
-      };
+          label-disconnected = "%{F#F0C674}%ifname%%{F#707880} disconnected";
+        };
 
-      "module/wlan" = {
-        "inherit" = "network-base";
-        interface-type = "wireless";
-        label-connected = "%{F#F0C674}%ifname%%{F-} %essid% %local_ip%";
-      };
+        "module/wlan" = {
+          "inherit" = "network-base";
+          interface-type = "wireless";
+          label-connected = "%{F#F0C674}%ifname%%{F-} %essid% %local_ip%";
+        };
 
-      "module/eth" = {
-        "inherit" = "network-base";
-        interface-type = "wired";
-        label-connected = "%{F#F0C674}%ifname%%{F-} %local_ip%";
-      };
+        "module/eth" = {
+          "inherit" = "network-base";
+          interface-type = "wired";
+          label-connected = "%{F#F0C674}%ifname%%{F-} %local_ip%";
+        };
 
-      "module/date" = {
-        type = "internal/date";
-        interval = 1;
+        "module/date" = {
+          type = "internal/date";
+          interval = 1;
 
-        date = "%H:%M";
-        date-alt = "%Y-%m-%d %H:%M:%S";
+          date = "%H:%M";
+          date-alt = "%Y-%m-%d %H:%M:%S";
 
-        label = "%date%";
-        label-foreground = ''''${colors.text}'';
-      };
+          label = "%date%";
+          label-foreground = ''''${colors.text}'';
+        };
 
-      "settings" = {
-        screenchange-reload = true;
-        pseudo-transparency = true;
+        "settings" = {
+          screenchange-reload = true;
+          pseudo-transparency = false;
+        };
       };
     };
   };
