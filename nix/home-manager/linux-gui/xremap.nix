39380c9166cf1925b598784570b36c2318d75171
Shinnosuke Sawada <6warashi9@gmail.com>
2023-10-01 21:36:23 +0900

    feat(xremap): add custom keys

--- nix/home-manager/linux-gui/xremap.nix
+++ nix/home-manager/linux-gui/xremap.nix
@@ -51,13 +51,15 @@ _: {
         {
           name = "Emacs emulation";
           remap = {
-            Ctrl_L-a = "Home";
-            Ctrl_L-e = "End";
-            Ctrl_L-f = "right";
+            Ctrl_L-a = "home";
             Ctrl_L-b = "left";
+            Ctrl_L-d = "delete";
+            Ctrl_L-e = "end";
+            Ctrl_L-f = "right";
+            Ctrl_L-h = "backspace";
+            Ctrl_L-k = ["Shift_R-end" "delete"];
             Ctrl_L-n = "down";
             Ctrl_L-p = "up";
-            Ctrl_L-h = "Backspace";
           };
           application = {
             not = ["Alacritty"];
@@ -66,9 +68,15 @@ _: {
         {
           name = "macOS enulation";
           remap = {
+            Super-a = "Ctrl_R-a";
             Super-f = "Ctrl_R-f";
-            Super-w = "Ctrl_R-w";
+            Super-h = "Ctrl_R-h";
+            Super-l = "Ctrl_R-l";
+            Super-q = "Ctrl_R-q";
             Super-t = "Ctrl_R-t";
+            Super-w = "Ctrl_R-w";
+            Super-LeftBrace = "Ctrl_R-LeftBrace";
+            Super-RightBrace = "Ctrl_R-RightBrace";
           };
         }
       ];
