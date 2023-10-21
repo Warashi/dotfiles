ef50f3120b590bc672ff89f0e452b1512c32fe90
Shinnosuke Sawada <6warashi9@gmail.com>
2023-10-21 12:11:18 +0900

    feat(nvim): バッファを移動したときもheirlineの表示が更新されるようにした

--- .config/nvim/lua/config/heirline/lsp.lua
+++ .config/nvim/lua/config/heirline/lsp.lua
@@ -3,6 +3,14 @@ return {
     return vim.fn["denops#plugin#is_loaded"]("lspoints") == 1 and #(vim.fn["lspoints#get_clients"]()) > 0
   end,
   update = { "User", pattern = { "LspointsAttach:*", "LspointsDetach:*", "DenopsPluginPost:lspoints" } },
+  init = function(self)
+    if not (rawget(self, "once")) then
+      vim.api.nvim_create_autocmd("BufWinEnter", {
+        callback = function() self._win_cache = nil end,
+      })
+      self.once = true
+    end
+  end,
   provider = function(_)
     local servers = {}
     for _, server in ipairs(vim.fn["lspoints#get_clients"]()) do
