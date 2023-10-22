4b292116a0eb738ba5ee794a24f3c55cd39b3230
Shinnosuke Sawada <6warashi9@gmail.com>
2023-10-22 11:41:02 +0900

    chore(efm-deadnix): change format

--- .config/nvim/lspoints/efmcmds/deadnix.ts
+++ .config/nvim/lspoints/efmcmds/deadnix.ts
@@ -6,8 +6,8 @@ export async function deadnix(_denops: Denops): Promise<ToolDefinition> {
   return {
     prefix: "deadnix",
     lintCommand:
-      `deadnix -o json \${INPUT} | jq -r '{file:.file,result:.results[]} | "\\(.file):\\(.result.line):\\(.result.column):\\(.result.endColumn): \\(.result.message)"'`,
-    lintFormats: ["%f:%l:%c:%k: %m"],
+      `deadnix -o json \${INPUT} | jq -r '{file:.file,result:.results[]} | "\\(.file):\\(.result.line):\\(.result.column):\\(.result.endColumn):\\(.result.message)"'`,
+    lintFormats: ["%f:%l:%c:%k:%m"],
     lintIgnoreExitCode: true,
   };
 }
