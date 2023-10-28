8ac83e0c1dbaf63c3d3b59de964292ee81134eaf
Shinnosuke Sawada <6warashi9@gmail.com>
2023-10-28 20:29:46 +0900

    feat(nvim): neovimからzk newと最後のファイルを編集できるようにした

---
+++ .config/nvim/denops/zk/main.ts
@@ -0,0 +1,31 @@
+import type { Denops } from "https://deno.land/x/denops_std@v5.0.1/mod.ts";
+
+function execute(...args: string[]): string {
+  const cmd = new Deno.Command("zk", { args });
+  const stdout = cmd.outputSync().stdout;
+  const decoder = new TextDecoder();
+  return decoder.decode(stdout);
+}
+
+export function main(denops: Denops): void {
+  denops.dispatcher = {
+    new: () =>
+      denops.cmd(`split ${execute("new", "--no-input", "--print-path")}`),
+    amend: () =>
+      denops.cmd(
+        `split ${
+          execute(
+            "list",
+            "--sort",
+            "modified",
+            "--limit",
+            "1",
+            "--format",
+            "{{abs-path}}",
+            "--no-pager",
+            "--quiet",
+          )
+        }`,
+      ),
+  };
+}
--- .config/nvim/dpprc/mappings.rc.vim
+++ .config/nvim/dpprc/mappings.rc.vim
@@ -3,3 +3,5 @@ nnoremap [t <Cmd>tabprevious<CR>
 nnoremap ]t <Cmd>tabnext<CR>
 nnoremap [T <Cmd>tabfirst<CR>
 nnoremap ]T <Cmd>tablast<CR>
+nnoremap <leader>zn <Cmd>call denops#request('zk', 'new', [])<CR>
+nnoremap <leader>zz <Cmd>call denops#request('zk', 'amend', [])<CR>
