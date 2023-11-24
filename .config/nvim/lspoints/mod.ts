e79430eb85f5a0316e57943461bfb617c4500b27
Shinnosuke Sawada <6warashi9@gmail.com>
2023-11-24 17:25:07 +0900

    style(lspoints): deno task format

--- .config/nvim/lspoints/mod.ts
+++ .config/nvim/lspoints/mod.ts
@@ -1,11 +1,11 @@
 export * from "./denols.ts";
-export * from "./fennells.ts";;
+export * from "./fennells.ts";
 export * from "./gopls.ts";
 export * from "./luals.ts";
 export * from "./nills.ts";
 export * from "./taplo.ts";
 export * from "./vimls.ts";
-export * from "./marksman.ts"
-export * from "./zk.ts"
+export * from "./marksman.ts";
+export * from "./zk.ts";
 
 export * as efmcmds from "./efmcmds/mod.ts";
--- .config/nvim/lspoints/taplo.ts
+++ .config/nvim/lspoints/taplo.ts
@@ -3,6 +3,6 @@ import type { Denops, StartOptions } from "./deps.ts";
 // deno-lint-ignore require-await
 export async function taplo(_denops: Denops): Promise<StartOptions> {
   return {
-      cmd: ["taplo", "lsp", "stdio"],
+    cmd: ["taplo", "lsp", "stdio"],
   };
 }
