f4c865ece165c3e60cc14d232e7e246544b24bd2
Shinnosuke Sawada <6warashi9@gmail.com>
2024-02-24 08:12:28 +0900

    feat(darwin): install pastebot

--- nix/nix-darwin/mas.nix
+++ nix/nix-darwin/mas.nix
@@ -1,8 +1,10 @@
 {
   DaisyDisk = 411643860;
+  Omnivore = 1564031042;
+  Pastebot = 1179623856;
   Tailscale = 1475387142;
-  Omnivo = 1564031042;
-  PiPifier = 1160374471;
-  "uBlacklist for Safari" = 1547912640;
+
   "1Password for Safari" = 1569813296;
+  "uBlacklist for Safari" = 1547912640;
+  PiPifier = 1160374471;
 }
