200eec87669d913fb1ed2ca612775b19fd8495ec
Shinnosuke Sawada <6warashi9@gmail.com>
2024-05-09 08:27:04 +0900

    feat(emacs): add packages

--- nix/emacs/ddskk/init.el
+++ nix/emacs/ddskk/init.el
@@ -1,3 +1,7 @@
 (setq skk-server-host "localhost"
       skk-server-portnum 1178
-      skk-server-report-response t)
+      skk-server-report-response t
+      skk-sticky-key ";"
+      skk-dcomp-activate t)
+(add-to-list 'skk-completion-prog-list
+             '(skk-comp-by-server-completion) t)
