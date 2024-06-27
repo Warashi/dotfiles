f21db02f945d74e1b469da33f005bf4ddd1ccdbc
Shinnosuke Dazai <shin@warashi.dev>
2024-06-28 00:44:11 +0900

    feat(linux-gui): user config

--- nix/nixos/gui/user.nix
+++ nix/nixos/gui/user.nix
@@ -1,7 +1,22 @@
-_: {
-  users.users.warashi = {
-    isNormalUser = true;
-    description = "warashi";
-    extraGroups = ["networkmanager" "wheel"];
+{pkgs, ...}: {
+  users = {
+    users = {
+      warashi = {
+        isNormalUser = true;
+        linger = true;
+        autoSubUidGidRange = true;
+        description = "warashi";
+        group = "warashi";
+        shell = pkgs.zsh;
+        extraGroups = [
+          "docker"
+          "networkmanager"
+          "wheel"
+        ];
+      };
+    };
+    groups = {
+      warashi = {};
+    };
   };
 }
