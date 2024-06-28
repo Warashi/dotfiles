5ac66066aaf2a06530d6d5ad5489ba87c56d0a5d
Shinnosuke Dazai <shin@warashi.dev>
2024-06-28 20:29:48 +0900

    fix(nvim): follow changes

--- .config/nvim/dpprc/hooks/ddu.lua
+++ .config/nvim/dpprc/hooks/ddu.lua
@@ -25,22 +25,12 @@ local function set_keymaps()
     group = group,
     pattern = "ddu-ff",
     callback = function()
-      local action = vim.fn["ddu#ui#ff#do_action"]
+      local action = vim.fn["ddu#ui#do_action"]
       local opts = { buffer = true }
       vim.keymap.set("n", "<CR>", function() action("itemAction") end, opts)
       vim.keymap.set("n", "<Space>", function() action("toggleSelectItem") end, opts)
       vim.keymap.set("n", "i", function() action("openFilterWindow") end, opts)
       vim.keymap.set("n", "q", function() action("quit") end, opts)
-    end,
-  })
-
-  vim.api.nvim_create_autocmd("FileType", {
-    group = group,
-    pattern = "ddu-ff-filter",
-    callback = function()
-      local opts = { buffer = true }
-      vim.keymap.set("i", "<CR>", "<Esc><Cmd>call ddu#ui#do_action('closeFilterWindow')<CR>", opts)
-      vim.keymap.set("n", "<CR>", "<Cmd>call ddu#ui#do_action('closeFilterWindow')<CR>", opts)
     end,
   })
 
@@ -54,17 +44,6 @@ local function set_keymaps()
       vim.keymap.set("n", "<Space>", function() action("toggleSelectItem") end, opts)
       vim.keymap.set("n", "o", function() action("expandItem", { mode = "toggle" }) end, opts)
       vim.keymap.set("n", "q", function() action("quit") end, opts)
-    end,
-  })
-
-  vim.api.nvim_create_autocmd("LspAttach", {
-    group = group,
-    callback = function(ev)
-      local opts = { buffer = ev.buf }
-      vim.keymap.set("n", "gd", function() start({ name = "lsp-definition" }) end, opts)
-      vim.keymap.set("n", "gr", function() start({ name = "lsp-references" }) end, opts)
-      vim.keymap.set("n", "<space>q", function() start({ name = "lsp-diagnostic" }) end, opts)
-      vim.keymap.set({ "n", "v" }, "<space>ca", function() start({ name = "lsp-codeAction" }) end, opts)
     end,
   })
 end
