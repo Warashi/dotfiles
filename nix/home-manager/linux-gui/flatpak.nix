fc13dea64a518928dec6ad67f387f7db1304cada
Shinnosuke Dazai <shin@warashi.dev>
2024-07-11 18:14:45 +0900

    feat(flatpak): overrides

--- nix/home-manager/linux-gui/flatpak.nix
+++ nix/home-manager/linux-gui/flatpak.nix
@@ -9,6 +9,19 @@ _: {
         "flathub:app/com.slack.Slack//stable"
         "flathub:app/com.discordapp.Discord//stable"
       ];
+      overrides = {
+        global = {
+          environment = {
+            MOZ_ENABLE_WAYLAND = 1;
+            ELECTRON_OZONE_PLATFORM_HINT = "auto";
+          };
+          sockets = [
+            "wayland"
+            "session-bus"
+            "system-bus"
+          ];
+        };
+      };
     };
   };
 }
