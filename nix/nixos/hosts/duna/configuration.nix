e54fd2ae77510b7118dd429d35fd800d8fa00fcd
Shinnosuke Dazai <shin@warashi.dev>
2025-01-11 13:58:04 +0900

    chore(duna): disable systemd-networkd wait online

--- nix/nixos/hosts/duna/configuration.nix
+++ nix/nixos/hosts/duna/configuration.nix
@@ -1,8 +1,4 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running ‘nixos-help’).
 {
-  config,
   pkgs,
   ...
 }:
@@ -21,4 +17,5 @@
 
   # Enable networking
   networking.networkmanager.enable = true;
+  systemd.network.wait-online.enable = false; # to avoid corruption between networkmanager and systemd-networkd
 }
