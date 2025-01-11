2a4e9591e7b22234e323968eb6394b3376784346
Shinnosuke Dazai <shin@warashi.dev>
2025-01-11 12:48:12 +0900

    fix(workbench): zen for aarch64 is broken

--- nix/nixos/hosts/workbench/configuration.nix
+++ nix/nixos/hosts/workbench/configuration.nix
@@ -3,7 +3,7 @@
   ...
 }:
 {
-  boot.kernelPackages = pkgs.linuxKernel.packages.linux_zen;
+  boot.kernelPackages = pkgs.linuxKernel.packages.linux_latest;
 
   # Bootloader.
   boot.loader.systemd-boot.enable = true;
